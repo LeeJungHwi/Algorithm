14f8ae03d759c5b85eae5924e7bf4b0fa0fb12e8
LeeJungHwi <101587101+LeeJungHwi@users.noreply.github.com>
2024-03-05 09:16:23 +0900

    [Silver I] Title: 절댓값 힙, Time: 16 ms, Memory: 2532 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.cc
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <fstream>
+using namespace std;
+
+// 절댓값 기준 구조체
+struct Num
+{
+	int number; // 숫자
+
+	// 생성자
+	Num() {}
+	Num(int n) : number(n) {}
+
+	// 절댓값 기준 내림차순
+	bool operator<(const Num &num)const
+	{
+		if (abs(number) != abs(num.number))
+		{
+			return abs(number) > abs(num.number);
+		}
+
+		return number > num.number;
+	}
+};
+
+// 절댓값 힙
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	//ifstream cin;
+	//cin.open("input.txt");
+
+	// 두가지연산
+	// 1. 0이아닌 x를 넣는다
+	// 2. 절댓값이 가장작은값을 출력하고 제거
+	// 절댓값이 작은값이 여러개면 그 중 작은수 출력하고 제거
+	// 빈배열일경우 0 출력
+
+	int n; // N 18
+	cin >> n;
+
+	priority_queue<Num> absMinHeap;
+
+	int op; // 연산
+
+	//	1
+	//	- 1
+	//	0
+	//	0
+	//	0
+	//	1
+	//	1
+	//	- 1
+	//	- 1
+	//	2
+	//	- 2
+	//	0
+	//	0
+	//	0
+	//	0
+	//	0
+	//	0
+	//	0
+	while (n--)
+	{
+		cin >> op;
+
+		// 0이 아니면 우선순위큐에 저장
+		if (op != 0)
+		{
+			absMinHeap.push(Num(op));
+
+			continue;
+		}
+
+		// 0이면 출력하고 pop
+		// 큐가 비었으면 0 출력
+		if (absMinHeap.empty())
+		{
+			cout << 0 << '\n';
+
+			continue;
+		}
+
+		cout << absMinHeap.top().number << '\n';
+		absMinHeap.pop();
+	}
+
+	return 0;
+}
