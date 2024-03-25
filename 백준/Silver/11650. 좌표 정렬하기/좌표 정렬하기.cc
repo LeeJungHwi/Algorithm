0a96c108d50084507954f48774deed32acf94315
JungHwi <101587101+LeeJungHwi@users.noreply.github.com>
2024-03-25 11:50:55 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 48 ms, Memory: 3688 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <fstream>
+using namespace std;
+
+// 정렬기준
+// x가 다르면 x 오름차순
+// x가 같으면 y 오름차순
+bool comp(const pair<int, int> &pos1, const pair<int, int> &pos2)
+{
+	if (pos1.first != pos2.first)
+	{
+		return pos1.first < pos2.first;
+	}
+
+	return pos1.second < pos2.second;
+}
+
+// 좌표 정렬하기
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	//ifstream cin;
+	//cin.open("input.txt");
+
+	int n; // N 5
+	cin >> n;
+
+	vector<pair<int, int> > posVec; // 좌표들 저장
+	pair<int, int> pos; // 좌표
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> pos.first >> pos.second;
+
+		posVec.push_back(pos); // 3 4, 1 1, 1 -1, 2 2, 3 3
+	}
+
+	// 정렬 후 출력
+	sort(posVec.begin(), posVec.end(), comp);
+
+	for (int i = 0; i < posVec.size(); i++)
+	{
+		cout << posVec[i].first << ' ' << posVec[i].second << '\n';
+	}
+
+	return 0;
+}
