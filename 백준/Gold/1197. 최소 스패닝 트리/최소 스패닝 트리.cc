8cfa1a97dfe82cd91f4d1890469cd5d686eaedcc
JungHwi <wnd4114294225@gmail.com>
2024-06-14 13:26:06 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 36 ms, Memory: 4496 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define home 0
+#define loop(v, s, e) for(int v = s; v < e; v++)
+#define elprint(a) cout << a << '\n'
+#define mat(t, v) vector<t> v
+
+// 집합 정보
+vector<int> unf(10001);
+
+// 간선 정보(가중치 오름차순)
+struct Edge
+{
+	int vertex1;
+	int vertex2;
+	int distance;
+
+	Edge() {}
+	Edge(int v1, int v2, int c) : vertex1(v1), vertex2(v2), distance(c) {}
+	bool operator<(Edge &edge) { return distance < edge.distance; }
+};
+
+// 정점이 속한 집합 번호 반환(루트)
+int Find(int v)
+{
+	if (v == unf[v]) return v;
+	return unf[v] = Find(unf[v]);
+}
+
+// 서로 다른 두 집합 결합
+void Union(int v1, int v2) { unf[v1] = v2; }
+
+// 최소 스패닝 트리
+int main()
+{
+	ios_base::sync_with_stdio(home);
+	cin.tie(home);
+	//ifstream cin;
+	//cin.open("input.txt");
+
+	int n, m;
+	cin >> n >> m;
+
+	mat(Edge, graph);
+
+	int v1, v2, c;
+	loop(i, 0, m)
+	{
+		cin >> v1 >> v2 >> c;
+		graph.push_back(Edge(v1 - 1, v2 - 1, c));
+	}
+	sort(graph.begin(), graph.end());
+	loop(i, 0, n) unf[i] = i;
+
+	// 최소 가중치
+	int minCost = 0;
+	loop(i, 0, m)
+	{
+		int fV1 = Find(graph[i].vertex1);
+		int fV2 = Find(graph[i].vertex2);
+
+		// 서로 다른 집합인 정점 중 최소 가중치 => 비용 누적, 유니온
+		if (fV1 != fV2)
+		{
+			minCost += graph[i].distance;
+			Union(fV1, fV2);
+		}
+	}
+
+	elprint(minCost);
+
+	return 0;
+}
