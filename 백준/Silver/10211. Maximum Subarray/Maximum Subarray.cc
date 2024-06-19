7d21a8c1c6c1ea53790315fbc7b9605f577997dc
JungHwi <wnd4114294225@gmail.com>
2024-06-19 15:40:42 +0900

    [Silver IV] Title: Maximum Subarray, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/10211. Maximum Subarray/Maximum Subarray.cc
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define home 0
+
+#define ll long long
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+#define aloop(t, v, s, c, e, i) for(t v = (s); v c (e); i)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define vec(t, v, r) vector<t> v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+#define dir vector<pii> checkDir = { {-1, 0}, {1, 0}, { 0, -1 }, { 0, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
+
+// Maximum Subarray
+int main()
+{
+	ios_base::sync_with_stdio(home);
+	cin.tie(home);
+	#ifndef ONLINE_JUDGE
+	ifstream cin("input.txt");
+	#endif
+
+	int tc;
+	cin >> tc;
+
+	while (tc--)
+	{
+		int n;
+		cin >> n;
+
+		vec(int, dp, n + 1);
+		int num;
+		loop(i, 1, n + 1)
+		{
+			cin >> num;
+			dp[i] = dp[i - 1] + num;
+		}
+
+		int maxSum = MIN;
+		loop(i, 1, n + 1) loop(j, i, n + 1)
+		{
+			int sum = dp[j] - dp[i - 1];
+			maxSum = max(maxSum, sum);
+		}
+
+		elp(maxSum);
+	}
+
+	return home;
+}
