f0589baa906db0d21dc4320737e52b4f9a89c908
JungHwi <wnd4114294225@gmail.com>
2024-06-19 16:44:40 +0900

    [Silver II] Title: 서로소 쌍, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/13412. 서로소 쌍/서로소 쌍.cc
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define home 0
+
+#define ll long long
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+#define aloop(t, v, s, l, c, r, i) for(t v = (s); (l) c (r); i)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define vec(t, v, r) vector<t> v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+#define dir vector<pii> checkDir = { {-1, 0}, {1, 0}, { 0, -1 }, { 0, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
+
+int GCD(int a, int b) { return cond(!(a % b), b, GCD(b, a % b)); }
+
+// 서로소 쌍
+int main()
+{
+	ios_base::sync_with_stdio(home);
+	cin.tie(home);
+	#ifndef ONLINE_JUDGE
+	ifstream cin("input.txt");
+	#endif
+
+	int tc;
+	cin >> tc;
+	while (tc--)
+	{
+		int n;
+		cin >> n;
+		int a = 0, b = 0, cnt = 0;
+		aloop(int, i, 1, i * i, <= , n, i++)
+		{
+			if (!(n % i))
+			{
+				a = i;
+				b = n / i;
+				if (GCD(a, b) == 1) cnt++;
+			}
+		}
+		elp(cnt);
+	}
+
+	return home;
+}
