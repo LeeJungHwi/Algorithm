db07f869071d39cf5f1dbeda6d4f3e45a0eb6864
JungHwi <wnd4114294225@gmail.com>
2024-06-22 12:16:07 +0900

    [Silver V] Title: 스네이크버드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.cc
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define home 0
+
+#define ll long long
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+#define aloop(t, v, s, c, e, i) for(t v = (s); v c (e); i)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define vec(t, v, r) vector<t> v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+#define dir vector<pii> checkDir = { {-1, 0}, {1, 0}, { 0, -1 }, { 0, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
+
+// 스네이크 버드
+int main()
+{
+	ios_base::sync_with_stdio(home);
+	cin.tie(home);
+	#ifndef ONLINE_JUDGE
+	ifstream cin("input.txt");
+	#endif
+
+	int n, m;
+	cin >> n >> m;
+	vec(int, v, n);
+	loop(i, home, n) cin >> v[i];
+	sort(all(v));
+	loop(i, home, n) if (v[i] <= m) m++; else break;
+	elp(m);
+
+	return home;
+}
