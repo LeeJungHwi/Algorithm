f7064c3ef035dcc187b3fdafd3ab309d064b9e11
JungHwi <wnd4114294225@gmail.com>
2024-06-22 12:49:29 +0900

    [Silver III] Title: 에너지 드링크, Time: 48 ms, Memory: 5220 KB -BaekjoonHub

---
+++ 백준/Silver/20115. 에너지 드링크/에너지 드링크.cc
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define home 0l
+
+#define ll long long
+#define ld long double
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+#define aloop(t, v, s, c, e, i) for(t v = (s); v c (e); i)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define vec(t, v, r) vector<t> v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+#define dir vector<pii> checkDir = { {-1, 0}, {1, 0}, { 0, -1 }, { 0, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
+
+struct Num
+{
+	ld number;
+
+	Num() {}
+	Num(ld n) : number(n) {}
+
+	bool operator<(const Num &num)const { return number > num.number; }
+};
+
+// 에너지 드링크
+int main()
+{
+	ios_base::sync_with_stdio(home);
+	cin.tie(home);
+	#ifndef ONLINE_JUDGE
+	ifstream cin("input.txt");
+	#endif
+
+	int n;
+	cin >> n;
+	priority_queue<Num> minHeap;
+	ld num;
+	loop(i, home, n) { cin >> num; minHeap.push(Num(num)); }
+	ld ans = home;
+	while (minHeap.size() > 1) { ans += minHeap.top().number / 2l; minHeap.pop(); }
+	ans += minHeap.top().number;
+	elp(ans);
+
+	return home;
+}
