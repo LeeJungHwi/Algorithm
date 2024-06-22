2b213f6db3941f43806e2f49b31d12586dded1c7
JungHwi <wnd4114294225@gmail.com>
2024-06-22 12:38:16 +0900

    [Silver V] Title: 거스름돈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.cc
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define home 0
+
+#define ll long long
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+#define aloop(t, v, s, c, e, i) for(t v = (s); v c (e); i)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define vec(t, v, r) vector<t> v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+#define dir vector<pii> checkDir = { {-1, 0}, {1, 0}, { 0, -1 }, { 0, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
+
+// 거스름돈
+int main()
+{
+	ios_base::sync_with_stdio(home);
+	cin.tie(home);
+	#ifndef ONLINE_JUDGE
+	ifstream cin("input.txt");
+	#endif
+
+	int n;
+	cin >> n;
+	if (n == 1 || n == 3) { elp(-1); return home; }
+	int ans = home, cnt = home;
+	while (5 * cnt <= n) cnt++; cnt--;
+	if (n % 2 && !(cnt % 2)) cnt--;
+	if (((n - 5 * cnt) % 2)) cnt--;
+	ans += (n - 5 * cnt) / 2; ans += cnt;
+	elp(ans);
+
+	return home;
+}
