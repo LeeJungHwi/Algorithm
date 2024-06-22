e37d1c1450486ee89c40e1a570725954a516031d
JungHwi <wnd4114294225@gmail.com>
2024-06-22 20:05:09 +0900

    [Silver V] Title: 사과 담기 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.cc
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define home 0
+
+#define ll long long
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+#define aloop(t, v, s, c, e, i) for(t v = (s); v c (e); i)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define vec(t, v, r) vector<t> v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+#define dir vector<pii> checkDir = { {-1, 0}, {1, 0}, { 0, -1 }, { 0, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
+
+// 사과 담기 게임
+int main()
+{
+	ios_base::sync_with_stdio(home);
+	cin.tie(home);
+	#ifndef ONLINE_JUDGE
+	ifstream cin("input.txt");
+	#endif
+
+	int n, m, k;
+	cin >> n >> m >> k;
+	if (k == 1) { elp(home); return home; }
+	vec(int, v, k);
+	loop(i, home, k) cin >> v[i];
+	int l = 1, r = m, ans = home;
+	loop(i, home, k)
+	{
+		int cnt = cond(v[i] < l, l - v[i], home);
+		if (cnt) { l -= cnt; r -= cnt; ans += cnt; continue; }
+		cnt = cond(v[i] > r, v[i] - r, home);
+		l += cnt; r += cnt; ans += cnt;
+	}
+	elp(ans);
+
+	return home;
+}
