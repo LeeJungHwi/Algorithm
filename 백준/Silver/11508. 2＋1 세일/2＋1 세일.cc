bb615d85ce26d1cb731f72d8f3c0046aa4269ccc
JungHwi <wnd4114294225@gmail.com>
2024-06-22 20:15:30 +0900

    [Silver IV] Title: 2+1 세일, Time: 24 ms, Memory: 3312 KB -BaekjoonHub

---
+++ 백준/Silver/11508. 2＋1 세일/2＋1 세일.cc
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define home 0
+
+#define ll long long
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+#define aloop(t, v, s, c, e, i) for(t v = (s); v c (e); i)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define vec(t, v, r) vector<t> v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+#define dir vector<pii> checkDir = { {-1, 0}, {1, 0}, { 0, -1 }, { 0, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
+
+// 2+1 세일
+int main()
+{
+	ios_base::sync_with_stdio(home);
+	cin.tie(home);
+	#ifndef ONLINE_JUDGE
+	ifstream cin("input.txt");
+	#endif
+
+	int n;
+	cin >> n;
+	vec(int, v, n);
+	loop(i, home, n) cin >> v[i];
+	if (n < 3) { int ans = home; loop(i, home, n) ans += v[i]; elp(ans); return home; }
+	priority_queue<int> maxHeap;
+	loop(i, home, n) maxHeap.push(v[i]);
+	ll ans = home;
+	while (maxHeap.size() > 2)
+	{
+		ans += maxHeap.top(); maxHeap.pop();
+		ans += maxHeap.top(); maxHeap.pop(); maxHeap.pop();
+	}
+	while (!maxHeap.empty()) { ans += maxHeap.top(); maxHeap.pop(); }
+	elp(ans);
+
+	return home;
+}
