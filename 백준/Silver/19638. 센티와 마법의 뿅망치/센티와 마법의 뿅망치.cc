fa7ddc325e4ffb4a9c09790e5341c1e0221a5931
JungHwi <wnd4114294225@gmail.com>
2024-06-24 17:58:01 +0900

    [Silver I] Title: 센티와 마법의 뿅망치, Time: 24 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/19638. 센티와 마법의 뿅망치/센티와 마법의 뿅망치.cc
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define home 0
+
+#define ll long long
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+#define aloop(t, v, s, c, e, i) for(t v = (s); v c (e); i)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define vec(t, v, r) vector<t> v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+#define gmat(t, v, r) vector<vector<t> > v((r))
+#define dir vector<pii> checkDir = { {-1, 0}, {1, 0}, { 0, -1 }, { 0, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
+
+// 센티와 마법의 뿅망치
+int main()
+{
+	ios_base::sync_with_stdio(home);
+	cin.tie(home);
+	#ifndef ONLINE_JUDGE
+	ifstream cin("input.txt");
+	#endif
+
+	int n, m, k;
+	cin >> n >> m >> k;
+	priority_queue<int> maxHeap;
+	int cnt = home;
+	int num;
+	loop(i, home, n)
+	{
+		cin >> num;
+		maxHeap.push(num);
+		if (num >= m) cnt++;
+	}
+	if (!cnt) { elp("YES"); elp(0); return home; }
+	int ans = home;
+	loop(i, home, k)
+	{
+		int n1 = maxHeap.top(); maxHeap.pop();
+		bool isHigh = cond(n1 >= m, true, false);
+		if (n1 > 1) n1 /= 2; ans++;
+		if (isHigh && n1 < m) cnt--;
+		maxHeap.push(n1);
+		if (!cnt) { elp("YES"); elp(ans); return home; }
+	}
+	elp("NO");
+	elp(maxHeap.top());
+
+	return home;
+}
