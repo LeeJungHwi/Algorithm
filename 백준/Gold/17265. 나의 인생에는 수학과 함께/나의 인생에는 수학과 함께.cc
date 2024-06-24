a570e00eba37086f0741ab9eebdf4014aa8d37c4
JungHwi <wnd4114294225@gmail.com>
2024-06-25 00:03:25 +0900

    [Gold V] Title: 나의 인생에는 수학과 함께, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/17265. 나의 인생에는 수학과 함께/나의 인생에는 수학과 함께.cc
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define home 0
+
+#define ll long long
+
+#define pii pair<int, int>
+#define pic pair<int, char>
+#define pll pair<ll, ll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+#define aloop(t, v, s, c, e, i) for(t v = (s); v c (e); i)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define vec(t, v, r) vector<t> v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+#define gmat(t, v, r) vector<vector<t> > v((r))
+#define dir vector<pii> checkDir = { {-1, 0}, {1, 0}, { 0, -1 }, { 0, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
+
+// 나의 인생에는 수학과 함께
+int main()
+{
+	ios_base::sync_with_stdio(home);
+	cin.tie(home);
+	#ifndef ONLINE_JUDGE
+	ifstream cin("input.txt");
+	#endif
+
+	int n;
+	cin >> n;
+	mat(char, graph, n, n);
+	stack<pair<pii, pic> > checkPos;
+	loop(i, home, n) loop(j, home, n) cin >> graph[i][j];
+
+	checkPos.push({ {home, home}, {graph[home][home] - '0', ' '} });
+	int maxP = MIN, minP = MAX;
+	while (!checkPos.empty())
+	{
+		pair<pii, pic> standard = checkPos.top();
+		checkPos.pop();
+
+		int ci = standard.lhs.lhs;
+		int cj = standard.lhs.rhs;
+		int cv = standard.rhs.lhs;
+		char co = standard.rhs.rhs;
+
+		if (ci == n - 1 && cj == n - 1)
+		{
+			maxP = max(maxP, cv);
+			minP = min(minP, cv);
+			continue;
+		}
+
+		if (ci + 1 < n)
+		{
+			if (isdigit(graph[ci + 1][cj]))
+			{
+				int temp = home;
+				if (co == '+') temp = cv + (graph[ci + 1][cj] - '0');
+				else if (co == '-') temp = cv - (graph[ci + 1][cj] - '0');
+				else if (co == '*') temp = cv * (graph[ci + 1][cj] - '0');
+				checkPos.push({ {ci + 1, cj},{temp, ' '} });
+			}
+			else checkPos.push({ { ci + 1, cj }, { cv, graph[ci + 1][cj]} });
+		}
+
+		if (cj + 1 >= n) continue;
+
+		if (isdigit(graph[ci][cj + 1]))
+		{
+			int temp = home;
+			if (co == '+') temp = cv + (graph[ci][cj + 1] - '0');
+			else if (co == '-') temp = cv - (graph[ci][cj + 1] - '0');
+			else if(co == '*') temp = cv * (graph[ci][cj + 1] - '0');
+			checkPos.push({ {ci, cj + 1},{temp, ' '} });
+			continue;
+		}
+		
+		checkPos.push({ { ci, cj + 1 }, { cv, graph[ci][cj + 1]} });
+	}
+	scp(maxP);
+	scp(minP);
+
+	return home;
+}
