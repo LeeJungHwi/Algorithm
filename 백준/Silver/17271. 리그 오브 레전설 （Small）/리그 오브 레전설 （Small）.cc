48926e55fa893105adc6db243350d9e77c483457
JungHwi <wnd4114294225@gmail.com>
2024-06-25 19:16:55 +0900

    [Silver II] Title: 리그 오브 레전설 (Small), Time: 4 ms, Memory: 3188 KB -BaekjoonHub

---
+++ 백준/Silver/17271. 리그 오브 레전설 （Small）/리그 오브 레전설 （Small）.cc
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define home 0
+
+#define ll long long
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+#define aloop(t, v, s, c, e, i) for(t v = (s); v c (e); i)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define vec(t, v, r) vector<t> v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+#define gmat(t, v, r) vector<vector<t> > v((r))
+#define dir vector<pii> cd = { {-1, 0}, {1, 0}, { 0, -1 }, { 0, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
+
+ll n, m;
+map<ll, ll> dp;
+
+ll DFS(ll cnt)
+{
+	if (cnt >= n) return cond(cnt == n, 1, home);
+	if (dp[cnt] > home) return dp[cnt];
+	return dp[cnt] = (DFS(cnt + 1) + cond(cnt + m <= n, DFS(cnt + m), home)) % 1000000007ll;
+}
+
+// 리그 오브 레전설 (Small)
+int main()
+{
+	ios_base::sync_with_stdio(home);
+	cin.tie(home);
+	#ifndef ONLINE_JUDGE
+	ifstream cin("input.txt");
+	#endif
+
+	cin >> n >> m;
+	ll ans = DFS(home);
+	elp(ans % 1000000007ll);
+
+	return home;
+}
