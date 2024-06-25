9bd9bc5b046282faaf66ae75a355341fbba11bd9
JungHwi <wnd4114294225@gmail.com>
2024-06-25 22:42:18 +0900

    [Silver II] Title: 아이들과 선물 상자, Time: 40 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/23757. 아이들과 선물 상자/아이들과 선물 상자.cc
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define home 0
+
+#define ll long long
+#define ld long double
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+#define aloop(t, v, s, c, e, i) for(t v = (s); v c (e); i)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define vec(t, v, r) vector<t> v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+#define gmat(t, v, r) vector<vector<t> > v((r))
+#define dir vector<pii> cd = { {-1, 0}, {1, 0}, { 0, -1 }, { 0, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
+
+// 아이들과 선물 상자
+int main()
+{
+	ios_base::sync_with_stdio(home);
+	cin.tie(home);
+	#ifndef ONLINE_JUDGE
+	ifstream cin("input.txt");
+	#endif
+
+	int n, m;
+	cin >> n >> m;
+	priority_queue<int> maxHeap;
+	int num;
+	loop(i, home, n) { cin >> num; maxHeap.push(num); }
+	loop(i, home, m)
+	{
+		cin >> num;
+		int n1 = maxHeap.top(); maxHeap.pop();
+		if (n1 - num < home) { elp(home); return home; }
+		if (n1 - num > home) maxHeap.push(n1 - num);
+	}
+	elp(1);
+
+	return home;
+}
