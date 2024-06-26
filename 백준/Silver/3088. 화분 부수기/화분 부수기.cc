dcfceedfda23d0a76a9c9ab2737fe05e5a70cc08
JungHwi <wnd4114294225@gmail.com>
2024-06-26 16:14:00 +0900

    [Silver II] Title: 화분 부수기, Time: 92 ms, Memory: 5780 KB -BaekjoonHub

---
+++ 백준/Silver/3088. 화분 부수기/화분 부수기.cc
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define home 0
+
+#define ll long long
+#define ld long double
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+#define aloop(t, v, s, c, e, i) for(t v = (s); v c (e); i)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define vec(t, v, r) vector<t> v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+#define gmat(t, v, r) vector<vector<t> > v((r))
+#define dir vector<pii> cd = { {-1, 0}, {1, 0}, { 0, -1 }, { 0, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
+
+struct Flower
+{
+	int num1, num2, num3;
+
+	Flower() {}
+	Flower(int n1, int n2, int n3) : num1(n1), num2(n2), num3(n3) {}
+};
+
+// 화분 부수기
+int main()
+{
+	ios_base::sync_with_stdio(home);
+	cin.tie(home);
+	#ifndef ONLINE_JUDGE
+	ifstream cin("input.txt");
+	#endif
+
+	int n;
+	cin >> n;
+	vec(Flower, v, n);
+	int maxN = MIN;
+	loop(i, home, n)
+	{
+		int m, k, l;
+		cin >> m >> k >> l;
+		v[i] = Flower(m, k, l);
+		maxN = max({ maxN, m, k, l });
+	}
+	vec(bool, vis, maxN + 1);
+	int ans = home;
+	loop(i, home, n)
+	{
+		if (!vis[v[i].num1] && !vis[v[i].num2] && !vis[v[i].num3]) ans++;
+		vis[v[i].num1] = vis[v[i].num2] = vis[v[i].num3] = true;
+	}
+	elp(ans);
+
+	return home;
+}
