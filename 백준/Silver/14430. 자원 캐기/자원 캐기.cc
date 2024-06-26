1a2577662da4f70cff49379f6bb280ce700f2664
JungHwi <wnd4114294225@gmail.com>
2024-06-26 20:14:22 +0900

    [Silver II] Title: 자원 캐기, Time: 4 ms, Memory: 2812 KB -BaekjoonHub

---
+++ 백준/Silver/14430. 자원 캐기/자원 캐기.cc
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define home 0
+
+#define ll long long
+#define ld long double
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+#define aloop(t, v, s, c, e, i) for(t v = (s); v c (e); i)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define vec(t, v, r) vector<t> v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+#define gmat(t, v, r) vector<vector<t> > v((r))
+#define dir vector<pii> cd = { {-1, 0}, {1, 0}, { 0, -1 }, { 0, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
+
+// 자원 캐기
+int main()
+{
+	ios_base::sync_with_stdio(home);
+	cin.tie(home);
+	#ifndef ONLINE_JUDGE
+	ifstream cin("input.txt");
+	#endif
+
+	int n, m;
+	cin >> n >> m;
+	mat(int, graph, n, m);
+	mat(int, dp, n, m);
+	loop(i, home, n) loop(j, home, m) cin >> graph[i][j];
+	dp[home][home] = graph[home][home];
+	loop(i, 1, m) dp[home][i] = dp[home][i - 1] + graph[home][i];
+	loop(i, 1, n) dp[i][home] = dp[i - 1][home] + graph[i][home];
+	loop(i, 1, n) loop(j, 1, m) dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]) + graph[i][j];
+	elp(dp[n - 1][m - 1]);
+
+	return home;
+}
