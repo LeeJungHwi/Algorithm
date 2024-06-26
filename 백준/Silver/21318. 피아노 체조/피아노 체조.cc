1e8f69ef98fa1afaa5386395ec1bcd0857c71fb6
JungHwi <wnd4114294225@gmail.com>
2024-06-26 19:35:23 +0900

    [Silver I] Title: 피아노 체조, Time: 40 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/21318. 피아노 체조/피아노 체조.cc
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define home 0
+
+#define ll long long
+#define ld long double
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+#define aloop(t, v, s, c, e, i) for(t v = (s); v c (e); i)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define vec(t, v, r) vector<t> v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+#define gmat(t, v, r) vector<vector<t> > v((r))
+#define dir vector<pii> cd = { {-1, 0}, {1, 0}, { 0, -1 }, { 0, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
+
+// 피아노 체조
+int main()
+{
+	ios_base::sync_with_stdio(home);
+	cin.tie(home);
+	#ifndef ONLINE_JUDGE
+	ifstream cin("input.txt");
+	#endif
+
+	int n;
+	cin >> n;
+	vec(int, dp, n);
+	int pre;
+	cin >> pre;
+	int num;
+	loop(i, 1, n)
+	{
+		cin >> num;
+		dp[i] = dp[i - 1];
+		if (pre > num) dp[i]++;
+		pre = num;
+	}
+	int m;
+	cin >> m;
+	loop(i, home, m)
+	{
+		int l, k;
+		cin >> l >> k;
+		int ans = dp[--k] - dp[--l];
+		elp(ans);
+	}
+
+	return home;
+}
