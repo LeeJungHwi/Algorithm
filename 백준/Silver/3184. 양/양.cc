bd76eda5b0aae474b261fdcea1bae4093271cecb
JungHwi <wnd4114294225@gmail.com>
2024-06-28 23:13:24 +0900

    [Silver I] Title: 양, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/3184. 양/양.cc
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define home 0
+
+#define ll long long
+#define ld long double
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+#define aloop(t, v, s, c, e, i) for(t v = (s); v c (e); i)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define vec(t, v, r) vector<t> v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+#define gmat(t, v, r) vector<vector<t> > v((r))
+#define dir vector<pii> cd = { {-1, 0}, {1, 0}, { 0, -1 }, { 0, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
+
+// 양
+int main()
+{
+	ios_base::sync_with_stdio(home);
+	cin.tie(home);
+	#ifndef ONLINE_JUDGE
+	ifstream cin("input.txt");
+	#endif
+
+	int n, m;
+	cin >> n >> m;
+	mat(char, graph, n, m);
+	queue<pii> cp;
+	dir;
+	loop(i, home, n) loop(j, home, m) cin >> graph[i][j];
+	int vAns = home, oAns = home;
+	loop(i, home, n) loop(j, home, m) if (graph[i][j] == 'v' || graph[i][j] == 'o')
+	{
+		cp.push({ i, j });
+		int vCnt = home, oCnt = home;
+		cond(graph[i][j] == 'v', vCnt++, oCnt++);
+		graph[i][j] = '#';
+		while (!cp.empty())
+		{
+			pii s = cp.front(); cp.pop();
+			loop(k, home, 4)
+			{
+				int ci = s.lhs + cd[k].lhs;
+				int cj = s.rhs + cd[k].rhs;
+				if (ci < home || cj < home || ci >= n || cj >= m) continue;
+				if (graph[ci][cj] == '#') continue;
+				if (graph[ci][cj] == 'v') vCnt++;
+				if (graph[ci][cj] == 'o') oCnt++;
+				cp.push({ ci, cj });
+				graph[ci][cj] = '#';
+			}
+		}
+		cond(vCnt >= oCnt, vAns += vCnt, oAns += oCnt);
+	}
+	scp(oAns); elp(vAns);
+
+	return home;
+}
