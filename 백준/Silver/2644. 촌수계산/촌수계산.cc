195d150f19d378810dcf3df2fc73bf3fa59bfa4e
JungHwi <wnd4114294225@gmail.com>
2024-07-05 08:34:09 +0900

    [Silver II] Title: 촌수계산, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2644. 촌수계산/촌수계산.cc
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define home 0
+
+#define ll long long
+#define ld long double
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define vec(t, v, r) vector<t> v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+#define gmat(t, v, r) vector<vector<t> > v((r))
+#define dir vector<pii> cd = { {-1, 0}, {1, 0}, { 0, -1 }, { 0, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
+
+// 촌수계산
+int main()
+{
+	ios_base::sync_with_stdio(home);
+	cin.tie(home);
+	#ifndef ONLINE_JUDGE
+	ifstream cin("input.txt");
+	#endif
+
+	int n, m, k, l;
+	cin >> n >> k >> l >> m;
+	gmat(int, graph, n);
+	vec(int, dis, n);
+	queue<int> cp;
+	int v1, v2;
+	loop(i, home, m)
+	{
+		cin >> v1 >> v2;
+		graph[v1 - 1].push_back(v2 - 1);
+		graph[v2 - 1].push_back(v1 - 1);
+	}
+	cp.push(--k);
+	dis[k] = 1;
+	while (!cp.empty())
+	{
+		int s = cp.front(); cp.pop();
+		loop(i, home, graph[s].size())
+		{
+			if (dis[graph[s][i]] > home) continue;
+			cp.push(graph[s][i]);
+			dis[graph[s][i]] = dis[s] + 1;
+		}
+	}
+	cond(!dis[--l], elp(-1), elp(--dis[l]));
+
+	return home;
+}
