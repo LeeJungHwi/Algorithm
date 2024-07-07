ddd3ac6bcdf19d6680fdbd6c7c21d9cdad9d46eb
JungHwi <wnd4114294225@gmail.com>
2024-07-08 07:40:32 +0900

    [Silver II] Title: Oil Deposits, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/4677. Oil Deposits/Oil Deposits.cc
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define home 0
+
+#ifdef ONLINE_JUDGE
+#define init ios_base::sync_with_stdio(home); cin.tie(home)
+#else
+#define init ios_base::sync_with_stdio(home); cin.tie(home); ifstream cin("input.txt")
+#endif
+
+#define ll long long
+#define ld long double
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define tvec(t, v) vector<t> v
+#define vec(t, v, r) vector<t> v((r))
+#define gmat(t, v, r) vector<vector<t> > v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+
+#define dir vector<pii> cd = { {-1, home}, {1, home}, { home, -1 }, { home, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
+
+// Oil Deposits
+int main()
+{
+	init; dir;
+	while (!home)
+	{
+		int n, m; cin >> n >> m;
+		if (!n && !m) return home;
+		mat(char, graph, n, m);
+		queue<pii> cp;
+		loop(i, home, n) loop(j, home, m) cin >> graph[i][j];
+		int ans = home;
+		loop(i, home, n) loop(j, home, m) if (graph[i][j] == '@')
+		{
+			cp.push({ i, j }); ++ans;
+			while (!cp.empty())
+			{
+				pii s = cp.front(); cp.pop();
+				loop(k, home, 8)
+				{
+					int ci = s.lhs + cd[k].lhs;
+					int cj = s.rhs + cd[k].rhs;
+					if (ci < home || cj < home || ci >= n || cj >= m) continue;
+					if (graph[ci][cj] == '*') continue;
+					cp.push({ ci, cj });
+					graph[ci][cj] = '*';
+				}
+			}
+		}
+		elp(ans);
+	}
+
+	return home;
+}
