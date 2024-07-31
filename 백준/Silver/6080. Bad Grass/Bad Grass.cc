fd973a799fe133db238448584b9c5d015f1cd622
JungHwi <wnd4114294225@gmail.com>
2024-07-31 20:33:17 +0900

    [Silver II] Title: Bad Grass, Time: 56 ms, Memory: 10220 KB -BaekjoonHub

---
+++ 백준/Silver/6080. Bad Grass/Bad Grass.cc
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define home 0
+
+#ifdef ONLINE_JUDGE
+#define init ios_base::sync_with_stdio(home); cin.tie(home)
+#else
+#define init ios_base::sync_with_stdio(home); cin.tie(home); ifstream cin("input.txt")
+#endif
+
+#define ll long long
+#define ld long double
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define tvec(t, v) vector<t> v
+#define vec(t, v, r) vector<t> v((r))
+#define gmat(t, v, r) vector<vector<t> > v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+
+#define dir vector<pii> cd = { {-1, home}, {1, home}, { home, -1 }, { home, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
+
+// Bad Grass
+int main()
+{
+	init;
+
+	int n, m; cin >> n >> m;
+	mat(int, graph, n, m);
+	queue<pii> cp; dir;
+	tvec(pii, sPos);
+	loop(i, home, n) loop(j, home, m)
+	{
+		cin >> graph[i][j];
+		if (graph[i][j] > home) sPos.push_back({ i, j });
+	}
+	int ans = home;
+	loop(i, home, sPos.size())
+	{
+		if (graph[sPos[i].lhs][sPos[i].rhs] == -1) continue;
+		cp.push(sPos[i]);
+		graph[sPos[i].lhs][sPos[i].rhs] = -1;
+		++ans;
+		while (!cp.empty())
+		{
+			pii s = cp.front(); cp.pop();
+			loop(j, home, 8)
+			{
+				int ci = s.lhs + cd[j].lhs;
+				int cj = s.rhs + cd[j].rhs;
+				if (ci < home || cj < home || ci >= n || cj >= m) continue;
+				if (graph[ci][cj] == -1) continue;
+				if (graph[ci][cj] == home) continue;
+				cp.push({ ci, cj });
+				graph[ci][cj] = -1;
+			}
+		}
+	}
+	elp(ans);
+
+	return home;
+}
