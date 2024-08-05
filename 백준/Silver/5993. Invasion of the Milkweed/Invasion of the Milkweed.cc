4b32d118683a601a03c494dc1474d27870a1c86c
JungHwi <wnd4114294225@gmail.com>
2024-08-05 22:32:11 +0900

    [Silver II] Title: Invasion of the Milkweed, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/5993. Invasion of the Milkweed/Invasion of the Milkweed.cc
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define home 0
+
+#ifdef ONLINE_JUDGE
+#define init ios_base::sync_with_stdio(home); cin.tie(home)
+#else
+#define init ios_base::sync_with_stdio(home); cin.tie(home); ifstream cin("input.txt")
+#endif
+
+#define ll long long
+#define ld long double
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define tvec(t, v) vector<t> v
+#define vec(t, v, r) vector<t> v((r))
+#define gmat(t, v, r) vector<vector<t> > v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+
+#define dir vector<pii> cd = { {-1, home}, {1, home}, { home, -1 }, { home, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
+
+// Invasion of the Milkweed
+int main()
+{
+	init;
+	
+	// sj : 1 => 0, 2 => 1, 3 => 2
+	// si : 1 => 2, 2 => 1, 3 => 0 
+
+	int n, m, si, sj; cin >> m >> n >> sj >> si;
+	mat(char, graph, n, m);
+	mat(int, dis, n, m);
+	queue<pii> cp; dir;
+	loop(i, home, n) loop(j, home, m) cin >> graph[i][j];
+	cp.push({ n - si, --sj });
+	dis[n - si][sj] = 1;
+	int ans = 1;
+	while (!cp.empty())
+	{
+		pii s = cp.front(); cp.pop();
+		loop(i, home, 8)
+		{
+			int ci = s.lhs + cd[i].lhs;
+			int cj = s.rhs + cd[i].rhs;
+			if (ci < home || cj < home || ci >= n || cj >= m) continue;
+			if (dis[ci][cj] > home) continue;
+			if (graph[ci][cj] == '*') continue;
+			cp.push({ ci, cj });
+			dis[ci][cj] = dis[s.lhs][s.rhs] + 1;
+			ans = max(ans, dis[ci][cj]);
+		}
+	}
+	elp(--ans);
+
+	return home;
+}
