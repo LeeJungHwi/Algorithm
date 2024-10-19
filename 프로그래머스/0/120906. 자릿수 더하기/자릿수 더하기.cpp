474a688a2b8d25478c1afb610edb28b6efe14844
JungHwi <wnd4114294225@gmail.com>
2024-10-19 17:50:51 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n)
+{
+    int answer = 0;
+    string temp = to_string(n);
+    for(int i = 0; i < temp.size(); i++) answer += (temp[i] - '0');
+    return answer;
+}
