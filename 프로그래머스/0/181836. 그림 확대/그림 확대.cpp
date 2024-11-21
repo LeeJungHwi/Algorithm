e3f135a9fdc29292895e74cea9d5e6eda8ac60dc
JungHwi <wnd4114294225@gmail.com>
2024-11-22 06:56:17 +0900

    [level 0] Title: 그림 확대, Time: 0.13 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(vector<string> picture, int k)
+{
+    vector<string> answer;
+    for(int i = 0; i < picture.size(); i++)
+    {
+        string temp = "";
+        for(int j = 0; j < picture[i].size(); j++)
+        {
+            for(int m = 0; m < k; m++)
+            {
+                temp += picture[i][j];
+            }
+        }
+        
+        for(int j = 0; j < k; j++) answer.push_back(temp);
+    }
+    return answer;
+}
