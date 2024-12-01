5963ba7ec66469e28e0d0cea22bdfd67aa9499a4
JungHwi <wnd4114294225@gmail.com>
2024-12-02 08:58:01 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string my_string, string is_suffix)
+{
+    for(int i = 0; i < my_string.size(); i++) if(my_string.substr(i, my_string.size() - i) == is_suffix) return 1;
+    return 0;
+}
