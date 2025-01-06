58cf776e2283baf02d5adb7a32c21bd00b3f0b70
JungHwi <wnd4114294225@gmail.com>
2025-01-07 05:39:53 +0900

    [Silver V] Title: 곰곰이의 식단 관리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/25193. 곰곰이의 식단 관리/곰곰이의 식단 관리.cc
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define home 0
+
+#ifdef ONLINE_JUDGE
+#define init ios_base::sync_with_stdio(home); cin.tie(home)
+#else
+#define init ios_base::sync_with_stdio(home); cin.tie(home); ifstream cin("input.txt")
+#endif
+
+#define ll long long
+#define ld long double
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define tvec(t, v) vector<t> v
+#define vec(t, v, r) vector<t> v((r))
+#define gmat(t, v, r) vector<vector<t> > v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+
+#define dir vector<pii> cd = { {-1, home}, {1, home}, { home, -1 }, { home, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
+
+// 곰곰이의 식단 관리
+int main()
+{
+	init;
+
+	int n; cin >> n;
+	int cnt = 0;
+	char food;
+	loop(i, home, n)
+	{
+		cin >> food;
+		if (food != 'C') cnt++;
+	}
+	elp(ceil((double)(n - cnt) / (double)(cnt + 1)));
+
+	return home;
+}
