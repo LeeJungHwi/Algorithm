2205cc7ae881c9dfedac91e5775111c3189c0cf3
JungHwi <wnd4114294225@gmail.com>
2025-01-08 13:44:31 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.cc
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define home 0
+
+#ifdef ONLINE_JUDGE
+#define init ios_base::sync_with_stdio(home); cin.tie(home)
+#else
+#define init ios_base::sync_with_stdio(home); cin.tie(home); ifstream cin("input.txt")
+#endif
+
+#define ll long long
+#define ld long double
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define tvec(t, v) vector<t> v
+#define vec(t, v, r) vector<t> v((r))
+#define gmat(t, v, r) vector<vector<t> > v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+
+#define dir vector<pii> cd = { {-1, home}, {1, home}, { home, -1 }, { home, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
+
+// 그룹 단어 체커
+int main()
+{
+	init;
+
+	int n; cin >> n;
+	string is;
+	char cur = 'A';
+	int ans = 0;
+
+	loop(i, home, n)
+	{
+		map<char, int> m;
+
+		cin >> is;
+		bool isGroup = true;
+		loop(j, home, is.size())
+		{
+			// 키가 있는 문자이면서 cur과 다른 문자면 X
+			if (m[is[j]] > 0 && cur != is[j])
+			{
+				isGroup = false;
+				break;
+			}
+
+			m[is[j]]++;
+			cur = is[j];
+		}
+
+		if (isGroup) ans++;
+	}
+
+	cout << ans << '\n';
+
+	return home;
+}
