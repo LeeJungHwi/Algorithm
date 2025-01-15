94a06673c4f3522b47b725de730aa80c4818ac30
JungHwi <wnd4114294225@gmail.com>
2025-01-15 15:29:52 +0900

    [Silver III] Title: 과제는 끝나지 않아!, Time: 124 ms, Memory: 6036 KB -BaekjoonHub

---
+++ 백준/Silver/17952. 과제는 끝나지 않아！/과제는 끝나지 않아！.cc
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define home 0
+
+#ifdef ONLINE_JUDGE
+#define init ios_base::sync_with_stdio(home); cin.tie(home)
+#else
+#define init ios_base::sync_with_stdio(home); cin.tie(home); ifstream cin("input.txt")
+#endif
+
+#define ll long long
+#define ld long double
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define tvec(t, v) vector<t> v
+#define vec(t, v, r) vector<t> v((r))
+#define gmat(t, v, r) vector<vector<t> > v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+
+#define dir vector<pii> cd = { {-1, home}, {1, home}, { home, -1 }, { home, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
+
+// 과제는 끝나지 않아!
+int main()
+{
+	init;
+
+	int n; cin >> n;
+	stack<pair<int, int> > s;
+	int command, score, time, ans = home;
+	loop(i, home, n)
+	{
+		cin >> command;
+		if (command == 1)
+		{
+			cin >> score >> time;
+			--time;
+			if (time == home) ans += score;
+			else s.push({ score, time });
+		}
+		else
+		{
+			if (!s.empty())
+			{
+				s.top().rhs--;
+				if (s.top().rhs == home)
+				{
+					ans += s.top().lhs;
+					s.pop();
+				}
+			}
+		}
+
+		//cout << s.top().lhs << ' ' << s.top().rhs << '\n';
+	}
+
+	cout << ans << '\n';
+
+	return home;
+}
