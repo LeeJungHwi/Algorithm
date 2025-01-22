35b46a34f685b9b4ab2fe86e3844471150e80ef7
JungHwi <wnd4114294225@gmail.com>
2025-01-22 16:35:39 +0900

    [Silver IV] Title: Generations of Tribbles, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/9507. Generations of Tribbles/Generations of Tribbles.cc
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define home 0
+
+#ifdef ONLINE_JUDGE
+#define init ios_base::sync_with_stdio(home); cin.tie(home)
+#else
+#define init ios_base::sync_with_stdio(home); cin.tie(home); ifstream cin("input.txt")
+#endif
+
+#define ll long long
+#define ld long double
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define tvec(t, v) vector<t> v
+#define vec(t, v, r) vector<t> v((r))
+#define gmat(t, v, r) vector<vector<t> > v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+
+#define dir vector<pii> cd = { {-1, home}, {1, home}, { home, -1 }, { home, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
+
+// Generations of Tribbles
+int main()
+{
+	init;
+
+	map<int, ll> dp;
+	dp[home] = dp[1] = 1; dp[2] = 2; dp[3] = 4;
+	loop(i, 4, 68) dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3] + dp[i - 4];
+
+	int tc; cin >> tc;
+	int num;
+	while (tc--)
+	{
+		cin >> num;
+		elp(dp[num]);
+	}
+
+	return home;
+}
