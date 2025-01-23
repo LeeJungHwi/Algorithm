f79ad61d0ead694ec86a2f8d1b8833eeaeff3d31
JungHwi <wnd4114294225@gmail.com>
2025-01-23 16:31:04 +0900

    [Silver IV] Title: 점화식, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/13699. 점화식/점화식.cc
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define home 0
+
+#ifdef ONLINE_JUDGE
+#define init ios_base::sync_with_stdio(home); cin.tie(home)
+#else
+#define init ios_base::sync_with_stdio(home); cin.tie(home); ifstream cin("input.txt")
+#endif
+
+#define ll long long
+#define ld long double
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define tvec(t, v) vector<t> v
+#define vec(t, v, r) vector<t> v((r))
+#define gmat(t, v, r) vector<vector<t> > v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+
+#define dir vector<pii> cd = { {-1, home}, {1, home}, { home, -1 }, { home, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
+
+// 점화식
+int main()
+{
+	init;
+
+	ll n; cin >> n;
+	map<ll, ll> dp;
+	dp[home] = 1;
+	loop(i, 1, n + 1)
+	{
+		ll temp = home;
+		rloop(j, i - 1, -1)
+		{
+			temp += dp[i - 1 - j] * dp[j];
+		}
+		dp[i] = temp;
+	}
+	cout << dp[n];
+	// 1 1 2 5, 0 * 3 + 1 * 2 + 2 * 1 + 3 * 0 = 5 + 2 + 2 + 5
+
+	return home;
+}
