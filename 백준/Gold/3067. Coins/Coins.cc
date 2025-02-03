b8a8690430a5fc7de8f6b33defc1440201070caa
JungHwi <wnd4114294225@gmail.com>
2025-02-03 20:04:50 +0900

    [Gold V] Title: Coins, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Gold/3067. Coins/Coins.cc
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define home 0
+
+#ifdef ONLINE_JUDGE
+#define init ios_base::sync_with_stdio(home); cin.tie(home)
+#else
+#define init ios_base::sync_with_stdio(home); cin.tie(home); ifstream cin("input.txt")
+#endif
+
+#define ll long long
+#define ld long double
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define tvec(t, v) vector<t> v
+#define vec(t, v, r) vector<t> v((r))
+#define gmat(t, v, r) vector<vector<t> > v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+
+#define dir vector<pii> cd = { {-1, home}, {1, home}, { home, -1 }, { home, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
+
+// Coins
+int main()
+{
+	init;
+
+	int tc; cin >> tc;
+
+	while (tc--)
+	{
+		int n; cin >> n;
+		vec(int, v, n);
+		loop(i, home, n) cin >> v[i];
+		int m; cin >> m;
+
+		vector<int> dp(m + 1, home); // dp[i] => i원을 만드는 방법의 수
+		dp[home] = 1; // 0원을 만드는 방법 => 동전 사용 X
+
+		// 각 동전을 돌면서
+		loop(i, home, n)
+		{
+			// (j - v[i])원을 만드는 방법의 수에 현재 가치의 동전 v[i]를 하나 사용하면 j원을 만들 수 있음
+			loop(j, v[i], m + 1)
+			{
+				dp[j] += dp[j - v[i]];
+			}
+		}
+
+		elp(dp[m]);
+	}
+
+	return home;
+}
