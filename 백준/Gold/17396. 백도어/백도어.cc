b72fcd9680928e7c47a3b998b3bfb7c8d5ac408e
JungHwi <wnd4114294225@gmail.com>
2025-02-05 00:29:40 +0900

    [Gold V] Title: 백도어, Time: 132 ms, Memory: 15580 KB -BaekjoonHub

---
+++ 백준/Gold/17396. 백도어/백도어.cc
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define home 0
+
+#ifdef ONLINE_JUDGE
+#define init ios_base::sync_with_stdio(home); cin.tie(home)
+#else
+#define init ios_base::sync_with_stdio(home); cin.tie(home); ifstream cin("input.txt")
+#endif
+
+#define ll long long
+#define ld long double
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define tvec(t, v) vector<t> v
+#define vec(t, v, r) vector<t> v((r))
+#define gmat(t, v, r) vector<vector<t> > v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+
+#define dir vector<pii> cd = { {-1, home}, {1, home}, { home, -1 }, { home, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
+
+// 백도어
+int main()
+{
+	init;
+
+	int n, m; cin >> n >> m;
+	vec(bool, ch, n);
+	bool ward;
+	loop(i, home, n)
+	{
+		cin >> ward;
+		ch[i] = ward;
+	}
+	gmat(pii, graph, n);
+	int v1, v2, c;
+	loop(i, home, m)
+	{
+		cin >> v1 >> v2 >> c;
+
+		graph[v1].push_back({ v2, c });
+		graph[v2].push_back({ v1, c });
+	}
+	vector<ll> dis(n, LLONG_MAX);
+
+	priority_queue<pii, vector<pii>, greater<pii>> pq;
+	pq.push({ home, home });
+	dis[home] = home;
+
+	while (!pq.empty())
+	{
+		int sp = pq.top().rhs;
+		int sc = pq.top().lhs;
+		pq.pop();
+
+		if (dis[sp] < sc) continue;
+
+		loop(i, home, graph[sp].size())
+		{
+			int cp = graph[sp][i].lhs;
+			int cc = graph[sp][i].rhs;
+
+			if (dis[cp] <= dis[sp] + cc) continue;
+
+			// 넥서스가 아니면서 시야가 보이는 곳은 X
+			if (cp != n - 1 && ch[cp] == 1) continue;
+
+			dis[cp] = dis[sp] + cc;
+			pq.push({ dis[cp], cp });
+		}
+	}
+	elp(cond(dis[--n] == LLONG_MAX, -1, dis[n]));
+
+	return home;
+}
