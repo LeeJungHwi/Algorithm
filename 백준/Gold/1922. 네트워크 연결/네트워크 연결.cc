c8e54a2664dfbcac6276b01b810c778bed3e7922
JungHwi <wnd4114294225@gmail.com>
2025-02-05 15:31:36 +0900

    [Gold IV] Title: 네트워크 연결, Time: 32 ms, Memory: 4460 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.cc
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define home 0
+
+#ifdef ONLINE_JUDGE
+#define init ios_base::sync_with_stdio(home); cin.tie(home)
+#else
+#define init ios_base::sync_with_stdio(home); cin.tie(home); ifstream cin("input.txt")
+#endif
+
+#define ll long long
+#define ld long double
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define tvec(t, v) vector<t> v
+#define vec(t, v, r) vector<t> v((r))
+#define gmat(t, v, r) vector<vector<t> > v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+
+#define dir vector<pii> cd = { {-1, home}, {1, home}, { home, -1 }, { home, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
+
+vector<int> unf(1001);
+
+int Find(int v)
+{
+	if (unf[v] == v) return v;
+	return unf[v] = Find(unf[v]);
+}
+
+void Union(int v1, int v2)
+{
+	v1 = Find(v1);
+	v2 = Find(v2);
+	if (v1 != v2) unf[v1] = v2;
+}
+
+struct Edge
+{
+	int v1, v2, c;
+	Edge() {}
+	Edge(int v1, int v2, int c) : v1(v1), v2(v2), c(c) {}
+	bool operator<(const Edge& e)const { return c < e.c; }
+};
+
+// 네트워크 연결
+int main()
+{
+	init;
+
+	int n, m; cin >> n >> m;
+	loop(i, home, n) unf[i] = i;
+	tvec(Edge, graph);
+	int v1, v2, c;
+	loop(i, home, m)
+	{
+		cin >> v1 >> v2 >> c;
+		graph.push_back(Edge(--v1, --v2, c));
+	}
+	sort(all(graph));
+
+	int minCost = home;
+	loop(i, home, graph.size())
+	{
+		v1 = Find(graph[i].v1);
+		v2 = Find(graph[i].v2);
+
+		if (v1 != v2)
+		{
+			minCost += graph[i].c;
+			Union(v1, v2);
+		}
+	}
+	elp(minCost);
+
+	return home;
+}
