05e709479e58e535efe8e8a5a11d9ff138256335
JungHwi <wnd4114294225@gmail.com>
2025-02-08 14:03:56 +0900

    [Silver III] Title: 모든 순열, Time: 16 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10974. 모든 순열/모든 순열.cc
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define home 0
+
+#ifdef ONLINE_JUDGE
+#define init ios_base::sync_with_stdio(home); cin.tie(home)
+#else
+#define init ios_base::sync_with_stdio(home); cin.tie(home); ifstream cin("input.txt")
+#endif
+
+#define ll long long
+#define ld long double
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define tvec(t, v) vector<t> v
+#define vec(t, v, r) vector<t> v((r))
+#define gmat(t, v, r) vector<vector<t> > v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+
+#define dir vector<pii> cd = { {-1, home}, {1, home}, { home, -1 }, { home, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
+
+int n;
+vector<int> v, ans;
+vector<bool> vis;
+
+void DFS(int L)
+{
+	if (L == n)
+	{
+		loop(i, home, ans.size()) scp(ans[i]);
+		cout << '\n';
+	}
+	else
+	{
+		loop(i, home, n)
+		{
+			if (!vis[i])
+			{
+				vis[i] = true;
+				ans.push_back(v[i]);
+				DFS(L + 1);
+				vis[i] = false;
+				ans.pop_back();
+			}
+		}
+	}
+}
+
+// 모든 순열
+int main()
+{
+	init;
+
+	cin >> n;
+
+	v.resize(n);
+	vis.resize(n);
+	loop(i, home, n) v[i] = i + 1;
+	DFS(home);
+
+	return home;
+}
