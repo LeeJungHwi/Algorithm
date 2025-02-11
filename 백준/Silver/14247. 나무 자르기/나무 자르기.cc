36b8143f19e577560f8bda38e5e34c0ebf2cf49c
JungHwi <wnd4114294225@gmail.com>
2025-02-12 02:00:50 +0900

    [Silver I] Title: 나무 자르기, Time: 12 ms, Memory: 2516 KB -BaekjoonHub

---
+++ 백준/Silver/14247. 나무 자르기/나무 자르기.cc
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define home 0
+
+#ifdef ONLINE_JUDGE
+#define init ios_base::sync_with_stdio(home); cin.tie(home)
+#else
+#define init ios_base::sync_with_stdio(home); cin.tie(home); ifstream cin("input.txt")
+#endif
+
+#define ll long long
+#define ld long double
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define tvec(t, v) vector<t> v
+#define vec(t, v, r) vector<t> v((r))
+#define gmat(t, v, r) vector<vector<t> > v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+
+#define dir vector<pii> cd = { {-1, home}, {1, home}, { home, -1 }, { home, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
+
+// 나무 자르기
+int main()
+{
+	init;
+
+	// 하루에 하나의 나무만 자를 수 있음
+	// 성장 속도가 빠른 나무를 최대한 키워서 잘라야 전체적인 나무의 개수에 이득
+
+	int n; cin >> n;
+	vec(pii, tree, n);
+	loop(i, home, n) cin >> tree[i].rhs;
+	loop(i, home, n) cin >> tree[i].lhs;
+	sort(all(tree));
+	
+	ll ans = home;
+	loop(i, home, n) ans += tree[i].rhs + tree[i].lhs * i;
+	elp(ans);
+
+	return home;
+}
