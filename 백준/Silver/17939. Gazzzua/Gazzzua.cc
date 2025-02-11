7445df6e0e5ad40aa2489fac20395577989f60e9
JungHwi <wnd4114294225@gmail.com>
2025-02-12 06:16:46 +0900

    [Silver I] Title: Gazzzua, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/17939. Gazzzua/Gazzzua.cc
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define home 0
+
+#ifdef ONLINE_JUDGE
+#define init ios_base::sync_with_stdio(home); cin.tie(home)
+#else
+#define init ios_base::sync_with_stdio(home); cin.tie(home); ifstream cin("input.txt")
+#endif
+
+#define ll long long
+#define ld long double
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define tvec(t, v) vector<t> v
+#define vec(t, v, r) vector<t> v((r))
+#define gmat(t, v, r) vector<vector<t> > v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+
+#define dir vector<pii> cd = { {-1, home}, {1, home}, { home, -1 }, { home, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
+
+// Gazzzua
+int main()
+{
+	init;
+
+	int n; cin >> n;
+	vec(int, price, n);
+	loop(i, home, n) cin >> price[i];
+
+	int highPrice = -2147000000, ans = home;
+
+	// 역순 => 항상 미래에 최고가격이 보장됨
+	// 현재 가격이 highPrice보다 작을때 이익 계산
+	rloop(i, n - 1, -1)
+	{
+		if (price[i] > highPrice) highPrice = price[i];
+		else ans += highPrice - price[i];
+	}
+	elp(ans);
+
+	return home;
+}
