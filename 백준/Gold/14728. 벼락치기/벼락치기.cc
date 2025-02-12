69359500c2226a6a34135ff07febfab59026af10
JungHwi <wnd4114294225@gmail.com>
2025-02-12 22:06:12 +0900

    [Gold V] Title: 벼락치기, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Gold/14728. 벼락치기/벼락치기.cc
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define home 0
+
+#ifdef ONLINE_JUDGE
+#define init ios_base::sync_with_stdio(home); cin.tie(home)
+#else
+#define init ios_base::sync_with_stdio(home); cin.tie(home); ifstream cin("input.txt")
+#endif
+
+#define ll long long
+#define ld long double
+
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define tvec(t, v) vector<t> v
+#define vec(t, v, r) vector<t> v((r))
+#define gmat(t, v, r) vector<vector<t> > v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+
+#define dir vector<pii> cd = { {-1, home}, {1, home}, { home, -1 }, { home, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
+
+// 벼락치기
+int main()
+{
+	init;
+
+	// 아이템 개수 제한 => 역순 냅색
+
+	int n, m; cin >> n >> m;
+	vec(pii, v, n);
+	loop(i, home, n) cin >> v[i].lhs >> v[i].rhs;
+	vec(int, dp, m + 1);
+
+	loop(i, home, n)
+	{
+		rloop(j, m, v[i].lhs - 1)
+		{
+			dp[j] = max(dp[j], dp[j - v[i].lhs] + v[i].rhs);
+		}
+	}
+	elp(dp[m]);
+
+	return home;
+}
