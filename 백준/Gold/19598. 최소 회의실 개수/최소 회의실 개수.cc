61b97291b704f31c96eeae05c305f57628cd4709
JungHwi <wnd4114294225@gmail.com>
2025-02-14 15:48:34 +0900

    [Gold V] Title: 최소 회의실 개수, Time: 40 ms, Memory: 3320 KB -BaekjoonHub

--- 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.cc
+++ 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.cc
@@ -3,47 +3,47 @@ using namespace std;
 
 #define home 0
 
+#ifdef ONLINE_JUDGE
+#define init ios_base::sync_with_stdio(home); cin.tie(home)
+#else
+#define init ios_base::sync_with_stdio(home); cin.tie(home); ifstream cin("input.txt")
+#endif
+
 #define ll long long
+#define ld long double
 
 #define pii pair<int, int>
 #define pll pair<ll, ll>
 
-#define loop(v, s, e) for(ll v = (s); v < (e); v++)
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
 #define rloop(v, s, e) for(int v = (s); v > (e); v--)
 #define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
 #define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
-#define aloop(t, v, s, c, e, i) for(t v = (s); v c (e); i)
 
 #define p(a) cout << (a)
 #define elp(a) cout << (a) << '\n'
 #define scp(a) cout << (a) << ' '
 
+#define tvec(t, v) vector<t> v
 #define vec(t, v, r) vector<t> v((r))
+#define gmat(t, v, r) vector<vector<t> > v((r))
 #define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
-#define dir vector<pii> checkDir = { {-1, 0}, {1, 0}, { 0, -1 }, { 0, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
 
+#define dir vector<pii> cd = { {-1, home}, {1, home}, { home, -1 }, { home, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
 #define lhs first
 #define rhs second
 
 #define cond(c, t, f) ((c) ? (t) : (f))
 #define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
 
 const int MAX = 2147000000;
 const int MIN = -2147000000;
 
-struct Num
+bool comp(const pii & p1, const pii & p2)
 {
-	int room;
-	ll number;
-
-	Num() {}
-	Num(int r, ll n) : room(r), number(n) {}
-
-	bool operator<(const Num &num)const { return number > num.number; }
-};
-
-bool comp(const pll &p1, const pll &p2)
-{
+	// 시작 시간 오름차순
+	// 끝나는 시간 오름차순
 	if (p1.lhs != p2.lhs) return p1.lhs < p2.lhs;
 	return p1.rhs < p2.rhs;
 }
@@ -51,33 +51,23 @@ bool comp(const pll &p1, const pll &p2)
 // 최소 회의실 개수
 int main()
 {
-	ios_base::sync_with_stdio(home);
-	cin.tie(home);
-	#ifndef ONLINE_JUDGE
-	ifstream cin("input.txt");
-	#endif
+	init;
 
-	int n;
-	cin >> n;
-
-	vec(pll, v, n);
+	int n; cin >> n;
+	vec(pii, v, n);
 	loop(i, home, n) cin >> v[i].lhs >> v[i].rhs;
 	sort(all(v), comp);
-	priority_queue<Num> minHeap;
-	int ans = 1;
-	minHeap.push(Num(ans, v[home].rhs));
-	loop(i, 1, n)
+
+	priority_queue<int, vector<int>, greater<int>> pq;
+	
+	loop(i, home, n)
 	{
-		if (minHeap.top().number <= v[i].lhs)
-		{
-			int n1 = minHeap.top().room;
-			minHeap.pop();
-			minHeap.push(Num(n1, v[i].rhs));
-			continue;
-		}
-		minHeap.push(Num(++ans, v[i].rhs));
+		pii cur = v[i];
+
+		if (!pq.empty() && pq.top() <= cur.lhs) pq.pop();
+		pq.push(cur.rhs);
 	}
-	elp(ans);
+	elp(pq.size());
 
 	return home;
 }
