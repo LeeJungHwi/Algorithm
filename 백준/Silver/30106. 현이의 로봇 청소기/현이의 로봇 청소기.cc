849e7b05b3161c5ff2ebd14374fe9d8977d6e0e5
JungHwi <wnd4114294225@gmail.com>
2025-02-21 20:29:09 +0900

    [Silver I] Title: 현이의 로봇 청소기, Time: 44 ms, Memory: 3084 KB -BaekjoonHub

--- 백준/Silver/30106. 현이의 로봇 청소기/현이의 로봇 청소기.cc
+++ 백준/Silver/30106. 현이의 로봇 청소기/현이의 로봇 청소기.cc
@@ -3,6 +3,12 @@ using namespace std;
 
 #define home 0
 
+#ifdef ONLINE_JUDGE
+#define init ios_base::sync_with_stdio(home); cin.tie(home)
+#else
+#define init ios_base::sync_with_stdio(home); cin.tie(home); ifstream cin("input.txt")
+#endif
+
 #define ll long long
 #define ld long double
 
@@ -13,17 +19,17 @@ using namespace std;
 #define rloop(v, s, e) for(int v = (s); v > (e); v--)
 #define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
 #define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
-#define aloop(t, v, s, c, e, i) for(t v = (s); v c (e); i)
 
 #define p(a) cout << (a)
 #define elp(a) cout << (a) << '\n'
 #define scp(a) cout << (a) << ' '
 
+#define tvec(t, v) vector<t> v
 #define vec(t, v, r) vector<t> v((r))
-#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
 #define gmat(t, v, r) vector<vector<t> > v((r))
-#define dir vector<pii> cd = { {-1, 0}, {1, 0}, { 0, -1 }, { 0, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
 
+#define dir vector<pii> cd = { {-1, home}, {1, home}, { home, -1 }, { home, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
 #define lhs first
 #define rhs second
 
@@ -37,35 +43,38 @@ const int MIN = -2147000000;
 // 현이의 로봇 청소기
 int main()
 {
-	ios_base::sync_with_stdio(home);
-	cin.tie(home);
-	#ifndef ONLINE_JUDGE
-	ifstream cin("input.txt");
-	#endif
+	init;
 
 	int n, m, k; cin >> n >> m >> k;
-	mat(pii, graph, n, m);
-	queue<pii> cp;
-	dir;
-	loop(i, home, n) loop(j, home, m) cin >> graph[i][j].lhs;
+	mat(int, graph, n, m);
+	mat(bool, vis, n, m);
+	loop(i, home, n) loop(j, home, m) cin >> graph[i][j];
+	queue<pii> cp; dir;
+
 	int ans = home;
-	loop(i, home, n) loop(j, home, m) if (!graph[i][j].rhs)
+	loop(i, home, n) loop(j, home, m) if (!vis[i][j])
 	{
 		cp.push({ i, j });
-		graph[i][j].rhs = 1;
-		++ans;
+		vis[i][j] = true;
+		ans++; // 작동 횟수 카운팅
+
 		while (!cp.empty())
 		{
-			pii s = cp.front(); cp.pop();
-			loop(l, home, 4)
+			int si = cp.front().lhs;
+			int sj = cp.front().rhs;
+			cp.pop();
+
+			loop(j, home, 4)
 			{
-				int ci = s.lhs + cd[l].lhs;
-				int cj = s.rhs + cd[l].rhs;
+				int ci = si + cd[j].lhs;
+				int cj = sj + cd[j].rhs;
+
 				if (ci < home || cj < home || ci >= n || cj >= m) continue;
-				if (graph[ci][cj].rhs) continue;
-				if (abs(graph[s.lhs][s.rhs].lhs - graph[ci][cj].lhs) > k) continue;
+				if (vis[ci][cj]) continue;
+				if (abs(graph[si][sj] - graph[ci][cj]) > k) continue; // 높이차가 k를 초과하면 X
+
 				cp.push({ ci, cj });
-				graph[ci][cj].rhs = 1;
+				vis[ci][cj] = true;
 			}
 		}
 	}
