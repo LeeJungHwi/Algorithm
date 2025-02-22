4682268b3ec1cd4afee85b489f43435cc92e5a7c
JungHwi <wnd4114294225@gmail.com>
2025-02-23 06:19:26 +0900

    [Gold IV] Title: Puyo Puyo, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/11559. Puyo Puyo/Puyo Puyo.cc
+++ 백준/Gold/11559. Puyo Puyo/Puyo Puyo.cc
@@ -1,186 +1,138 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <string>
-#include <fstream>
+#include <bits/stdc++.h>
 using namespace std;
+
+#define home 0
+
+#ifdef ONLINE_JUDGE
+#define init ios_base::sync_with_stdio(home); cin.tie(home)
+#else
+#define init ios_base::sync_with_stdio(home); cin.tie(home); ifstream cin("input.txt")
+#endif
+
+#define ll long long
+#define ld long double
+
+#define pii pair<int, int>
+#define piii pair<int, pii>
+#define pll pair<ll, ll>
+#define plll pair<ll, pll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define tvec(t, v) vector<t> v
+#define vec(t, v, r) vector<t> v((r))
+#define gmat(t, v, r) vector<vector<t> > v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+#define smat(t, v, r, c, s) vector<vector<vector<t> > > v((r), vector<vector<t>>((c), vector<t>((s))))
+
+#define dir vector<pii> cd = { {-1, home}, {1, home}, { home, -1 }, { home, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+#define kdir vector<pii> kcd = { {-1, -2}, {-2, -1}, { -2, 1 }, { -1, 2 }, { 1, -2 }, { 2, -1 }, { 1, 2 }, { 2, 1 } }
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
 
 // Puyo Puyo
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	//ifstream cin;
-	//cin.open("input.txt");
+	init;
 
-	// 퍼즐게임
-	// 같은색 뿌요가 4개이상 연결되어있으면 한꺼번에 없어짐 
-	// 뿌요들이 없어지고 위에 다른색의 뿌요가있으면 아래로 내려옴
-	// 연쇄가 몇번 일어나는지 출력하기
+	// 1.같은 색 뿌요가 4개 이상 상하좌우로 연결 되어 있다면 한번에 없어짐 => 한번에 없애야함
+	// 2.위에 남은 뿌요는 경계 또는 다른 뿌요를 만날때까지 아래로 내려옴 => 역순으로 돌면서 내리고 내릴 때 이전 값은 .으로 채움
+	// 더 이상 4개 이상 연결되지 않을때까지 1, 2 반복
 
-	vector<vector<char> > graph(12, vector<char>(6)); // 그래프
-	vector<vector<char> > vis(12, vector<char>(6)); // 방문체크
-	queue<pair<int, int> > checkPos; // 체크 할 위치
-	vector<pair<int, int> > checkDir; // 상하좌우
-	checkDir.push_back({ -1, 0 });
-	checkDir.push_back({ 1, 0 });
-	checkDir.push_back({ 0, -1 });
-	checkDir.push_back({ 0, 1 });
-
-	//	......
-	//	......
-	//	......
-	//	......
-	//	......
-	//	......
-	//	......
-	//	......
-	//	.Y....
-	//	.YG...
-	//	RRYG..
-	//	RRYGG.
-	string inputString;
-
-	for (int i = 0; i < 12; i++)
+	mat(char, graph, 12, 6);
+	mat(bool, vis, 12, 6);
+	queue<pii> cp; dir;
+	int startI = -1; // 시작 열 => startI열 부터 탐색
+	loop(i, home, 12) loop(j, home, 6)
 	{
-		cin >> inputString;
-
-		for (int j = 0; j < 6; j++)
-		{
-			graph[i][j] = inputString[j];
-		}
+		cin >> graph[i][j];
+		if (graph[i][j] != '.' && startI == -1) startI = i;
 	}
 
-	// 1.그래프를 정방향으로 탐색하면서 같은색깔 4개이상이 되는 영역을 찾으면 빈칸으로 없앰
-	// 2.그래프를 역방향으로 탐색하면서 뿌요뿌요를 만나면 뿌요뿌요나 경계를 만날때까지 아래로 이동시킴
-	// 1, 2를 반복하다가 연쇄가 일어나지않았다면 반복종료
+	// 뿌요가 없는 경우
+	if (startI == -1) { elp(home); return home; }
 
-	int comboCnt = 0; // 연쇄 횟수
-
+	int ans = home; // 연쇄 횟수
 	while (true)
 	{
-		bool isCombo = false; // 연쇄가 일어났는지 체크
+		// 1.같은 색 뿌요가 4개 이상 상하좌우로 연결 되어 있다면 한번에 없어짐 => 한번에 없애야함
 
-		// 1.그래프를 정방향으로 탐색하면서 같은색깔 4개이상이 되는 영역을 찾으면 빈칸으로 없앰
-		for (int i = 0; i < 12; i++)
+		int removeCnt = home; // 없앤 횟수
+		loop(i, startI, 12) loop(j, home, 6) if (!vis[i][j] && graph[i][j] != '.')
 		{
-			for (int j = 0; j < 6; j++)
+			cp.push({ i, j });
+			vis[i][j] = true;
+			tvec(pii, removePuyo); // 한번에 없앨 푸요 위치 저장
+			removePuyo.push_back({ i, j });
+
+			// 현재 뿌요와 같은 색 뿌요가 4개 이상 상하좌우로 연결 되어 있는지 확인
+			while (!cp.empty())
 			{
-				if (graph[i][j] != '.' && !vis[i][j])
+				int si = cp.front().lhs;
+				int sj = cp.front().rhs;
+				cp.pop();
+
+				loop(k, home, 4)
 				{
-					// 그래프를 돌면서 뿌요뿌요를 만나면서 방문하지 않았으면 BFS 돌리기
+					int ci = si + cd[k].lhs;
+					int cj = sj + cd[k].rhs;
 
-					vector<pair<int, int> > removePuyo; // 없앨 푸요푸요 저장
-					int sameColorPuyoCnt = 1; // 같은색깔 뿌요뿌요 개수
-					checkPos.push({ i, j });
-					vis[i][j] = true;
-					removePuyo.push_back({ i, j });
+					if (ci < home || cj < home || ci >= 12 || cj >= 6) continue;
+					if (vis[ci][cj]) continue;
+					if (graph[ci][cj] != graph[si][sj]) continue; // 다른 색 뿌요거나 빈 공간인 경우 X
 
-					while (!checkPos.empty()) // 큐가 빌때까지
-					{
-						// 뿌요뿌요 꺼냄
-						pair<int, int> standardPos = checkPos.front();
-						checkPos.pop();
-
-						// 상하좌우
-						for (int k = 0; k < 4; k++)
-						{
-							// 체크 할 위치
-							int checkI = standardPos.first + checkDir[k].first;
-							int checkJ = standardPos.second + checkDir[k].second;
-
-							// 경계체크
-							if (checkI < 0 || checkJ < 0 || checkI >= 12 || checkJ >= 6)
-							{
-								continue;
-							}
-
-							// 방문체크
-							if (vis[checkI][checkJ])
-							{
-								continue;
-							}
-
-							// 빈칸체크
-							if (graph[checkI][checkJ] == '.')
-							{
-								continue;
-							}
-
-							// 다른색깔 뿌요뿌요 체크
-							if (graph[checkI][checkJ] != graph[standardPos.first][standardPos.second])
-							{
-								continue;
-							}
-
-							// 같은색깔 뿌요뿌요를 만났으면 큐에저장, 방문체크, 같은색뿌요카운팅, 없앨 푸요푸요에 저장
-							checkPos.push({ checkI, checkJ });
-							vis[checkI][checkJ] = true;
-							sameColorPuyoCnt++;
-							removePuyo.push_back({ checkI, checkJ });
-						}
-					}
-
-					// 같은색 뿌요뿌요가 4개이상이면 빈칸으로 바꿔서 없애고 연쇄가 일어났음 체크
-					if (sameColorPuyoCnt >= 4)
-					{
-						for (int k = 0; k < removePuyo.size(); k++)
-						{
-							graph[removePuyo[k].first][removePuyo[k].second] = '.';
-						}
-
-						isCombo = true;
-					}
+					cp.push({ ci, cj });
+					vis[ci][cj] = true;
+					removePuyo.push_back({ ci, cj });
 				}
 			}
+
+			// 크기가 4보다 작으면 연쇄 X
+			if (removePuyo.size() < 4) continue;
+
+			// 같은 색 뿌요가 4개 이상 연결 되어 있으면 뿌요 한번에 없앰
+			loop(k, home, removePuyo.size()) graph[removePuyo[k].lhs][removePuyo[k].rhs] = '.';
+			removeCnt++;
 		}
 
-		// 2.그래프를 역방향으로 탐색하면서 뿌요뿌요를 만나면 뿌요뿌요나 경계를 만날때까지 아래로 이동시킴
-		for (int i = 10; i > -1; i--)
+		// 더 이상 연쇄가 일어 나지 않으면 종료
+		if (removeCnt == home) break;
+
+		// 없앤 뿌요가 있으면 1연쇄 카운팅하고 방문체크 초기화
+		ans++;
+		loop(i, startI, 12) loop(j, home, 6) vis[i][j] = false;
+
+		// 2.위에 남은 뿌요는 경계 또는 다른 뿌요를 만날때까지 아래로 내려옴 => 역순으로 돌면서 내리고 내릴 때 이전 값은 .으로 채움
+		rloop(i, 10, startI - 1) loop(j, home, 6) if (graph[i][j] != '.')
 		{
-			for (int j = 0; j < 6; j++)
+			int si = i;
+
+			while (true)
 			{
-				if (graph[i][j] != '.') // 뿌요뿌요를 만나면
-				{
-					for (int k = i; k < 11; k++) // 경계 전까지
-					{
-						if (graph[k + 1][j] != '.') // 뿌요뿌요를 만날때까지
-						{
-							break;
-						}
+				// 경계 또는 다른 뿌요를 만나면 X
+				if (++si >= 12 || graph[si][j] != '.') break;
 
-						// 아래로 이동시키고 기존 뿌요뿌요는 빈칸으로
-						graph[k + 1][j] = graph[k][j];
-						graph[k][j] = '.';
-					}
-				}
-			}
-		}
-
-		// 연쇄가 일어나지않았으면 종료
-		if (!isCombo)
-		{
-			break;
-		}
-
-		// 연쇄가 일어난 횟수 증가
-		// 동시에 여러 그룹이 터져도 하나의 연쇄로 취급하므로
-		// 여기서 증가시켜줘야함
-		comboCnt++;
-
-		// vis 초기화
-		for (int i = 0; i < 12; i++)
-		{
-			for (int j = 0; j < 6; j++)
-			{
-				vis[i][j] = false;
+				// 아래로 내리면서 이전 값은 .으로 채움
+				graph[si][j] = graph[si - 1][j];
+				graph[si - 1][j] = '.';
 			}
 		}
 	}
+	elp(ans);
 
-	// 연쇄가 몇번 일어나는지 출력하기
-	cout << comboCnt << '\n';
-
-	return 0;
+	return home;
 }
