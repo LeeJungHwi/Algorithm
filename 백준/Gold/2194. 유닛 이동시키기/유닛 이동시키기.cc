43d719a0cdc2c84a194c2c9b2a7fdf5773122c59
JungHwi <wnd4114294225@gmail.com>
2025-02-22 23:46:36 +0900

    [Gold IV] Title: 유닛 이동시키기, Time: 8 ms, Memory: 4140 KB -BaekjoonHub

--- 백준/Gold/2194. 유닛 이동시키기/유닛 이동시키기.cc
+++ 백준/Gold/2194. 유닛 이동시키기/유닛 이동시키기.cc
@@ -1,205 +1,116 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <string>
-#include <map>
-#include <fstream>
+#include <bits/stdc++.h>
 using namespace std;
+
+#define home 0
+
+#ifdef ONLINE_JUDGE
+#define init ios_base::sync_with_stdio(home); cin.tie(home)
+#else
+#define init ios_base::sync_with_stdio(home); cin.tie(home); ifstream cin("input.txt")
+#endif
+
+#define ll long long
+#define ld long double
+
+#define pii pair<int, int>
+#define piii pair<int, pii>
+#define pll pair<ll, ll>
+#define plll pair<ll, pll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define tvec(t, v) vector<t> v
+#define vec(t, v, r) vector<t> v((r))
+#define gmat(t, v, r) vector<vector<t> > v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+#define smat(t, v, r, c, s) vector<vector<vector<t> > > v((r), vector<vector<t>>((c), vector<t>((s))))
+
+#define dir vector<pii> cd = { {-1, home}, {1, home}, { home, -1 }, { home, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+#define kdir vector<pii> kcd = { {-1, -2}, {-2, -1}, { -2, 1 }, { -1, 2 }, { 1, -2 }, { 2, -1 }, { 1, 2 }, { 2, 1 } }
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
 
 // 유닛 이동시키기
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	//ifstream cin;
-	//cin.open("input.txt");
+	init;
 
-	int n, m; // N, M 5, 5
-	cin >> n >> m;
+	// 기준이 유닛 좌상 => 방문체크는 좌상에서 상하좌우 한칸만 확인
+	// 다음 좌상의 첫행, 첫열, 마지막행, 마지막열만 한칸 씩 진행하면서 확인 => 사이는 이미 확인됨
+	// 경계 또는 장애물을 만나면 갈 수 없음
 
-	int a, b; // A, B 2, 2
-	cin >> a >> b;
+	int n, m, h, w, k; cin >> n >> m >> h >> w >> k;
+	mat(int, graph, n, m);
+	mat(int, dis, n, m);
+	queue<pii> cp; dir;
+	pii block, sPos, ePos;
 
-	int k; // K 3
-	cin >> k;
-
-	vector<vector<int> > graph(n, vector<int>(m)); // 그래프
-	vector<vector<int> > dis(n, vector<int>(m)); // 거리
-	queue<pair<int, int> > checkPos; // 체크 할 위치
-	vector<pair<int, int> > checkDir; // 상하좌우
-	checkDir.push_back({ -1, 0 });
-	checkDir.push_back({ 1 * a, 0 });
-	checkDir.push_back({ 0, -1 });
-	checkDir.push_back({ 0, 1 * b});
-
-	// 장애물
-	//	2 2
-	//	3 2
-	//	3 3
-	int y, x;
-
-	for (int i = 0; i < k; i++)
+	// k개의 장애물 설치
+	loop(i, home, k)
 	{
-		cin >> y >> x;
-
-		graph[y - 1][x - 1] = 1;
+		cin >> block.lhs >> block.rhs;
+		graph[--block.lhs][--block.rhs] = 1;
 	}
 
-	//for (auto a : graph)
-	//{
-	//	for (auto b : a)
-	//	{
-	//		cout << b << ' ';
-	//	}
-	//	cout << '\n';
-	//}
+	cin >> sPos.lhs >> sPos.rhs >> ePos.lhs >> ePos.rhs;
+	sPos.lhs--; sPos.rhs--; ePos.lhs--; ePos.rhs--;
+	cp.push(sPos);
+	dis[sPos.lhs][sPos.rhs] = 1;
 
-	// 시작위치, 도착위치
-	pair<int, int> startPos; // 4 1
-	cin >> startPos.first >> startPos.second;
-	checkPos.push({ startPos.first - 1, startPos.second - 1 });
-	dis[startPos.first - 1][startPos.second - 1] = 1;
-
-	//cout << startPos.first - 1 << ' ' << startPos.second - 1 << '\n';
-
-	pair<int, int> endPos; // 1 4
-	cin >> endPos.first >> endPos.second;
-
-	//cout << endPos.first - 1 << ' ' << endPos.second - 1 << '\n';
-
-	// BFS
-	while (!checkPos.empty()) // 큐가 빌때까지
+	while (!cp.empty())
 	{
-		// 기준위치 꺼냄
-		pair<int, int> standardPos = checkPos.front();
-		checkPos.pop();
+		int si = cp.front().lhs;
+		int sj = cp.front().rhs;
+		cp.pop();
 
-		// 유닛위치가 유닛전체부분에서 좌상위치
-		// 상하좌우 체크할 때 상하는 b만큼 좌우는 a만큼 체크해야함
-		for (int i = 0; i < 4; i++)
+		loop(i, home, 4)
 		{
-			// 경계체크, 방문체크 => 좌상에서 상하좌우 한칸씩만 확인하면됨
-			if (i % 2 == 0)
+			// 기준이 유닛 좌상 => 방문체크는 좌상에서 상하좌우 한칸만 확인
+			int ci = si + cd[i].lhs;
+			int cj = sj + cd[i].rhs;
+			if (ci < home || cj < home || ci >= n || cj >= m) continue;
+			if (dis[ci][cj] > home) continue;
+
+			// 다음 좌상의 첫행, 첫열, 마지막행, 마지막열만 확인 => 사이는 이미 확인됨
+			bool isMove = true;
+			loop(j, home, cond(i < 2, w, h))
 			{
-				int checkI = standardPos.first + checkDir[i].first;
-				int checkJ = standardPos.second + checkDir[i].second;
-				if (checkI < 0 || checkJ < 0 || checkI >= n || checkJ >= m)continue;
-				if (dis[checkI][checkJ] > 0) continue;
-			}
-			else
-			{
-				if (i == 1)
+				// 두 번째 항 => 0이면 첫 행, 1이면 마지막 행
+				// 세 번째 항 => 좌우이므로 하로 한칸 씩 이동
+				int cii = si + cd[i].lhs * cond(i == 1, h, 1) + cond(i >= 2, j, home);
+
+				// 두 번째 항 => 2면 첫 열, 3이면 마지막 열
+				// 세 번째 항 => 상하이므로 우로 한칸 씩 이동
+				int cjj = sj + cd[i].rhs * cond(i == 3, w, 1) + cond(i < 2, j, home);
+
+				// 경계 또는 벽을 만나면 X
+				if (cii < home || cjj < home || cii >= n || cjj >= m || graph[cii][cjj] == 1)
 				{
-					int checkI = standardPos.first + checkDir[i].first / a;
-					int checkJ = standardPos.second + checkDir[i].second;
-					if (checkI < 0 || checkJ < 0 || checkI >= n || checkJ >= m)continue;
-					if (dis[checkI][checkJ] > 0) continue;
-				}
-				else if (i == 3)
-				{
-					int checkI = standardPos.first + checkDir[i].first;
-					int checkJ = standardPos.second + checkDir[i].second / b;
-					if (checkI < 0 || checkJ < 0 || checkI >= n || checkJ >= m)continue;
-					if (dis[checkI][checkJ] > 0) continue;
-				}
-			}
-
-			bool isMove = true; // 움직일수있는지 체크
-
-			// 상하
-			if (i < 2)
-			{
-				// b만큼 우로 가면서 상하 체크
-				// 하나라도 경계를 만나거나 벽에 막히면 isMove false, break
-				for (int j = 0; j < b; j++)
-				{
-					// 체크 할 위치
-					int checkI = standardPos.first + checkDir[i].first;
-					int checkJ = standardPos.second + checkDir[i].second + j;
-
-					// 경계체크
-					if (checkI < 0 || checkJ < 0 || checkI >= n || checkJ >= m)
-					{
-						isMove = false;
-
-						break;
-					}
-
-					// 장애물체크
-					if (graph[checkI][checkJ] == 1)
-					{
-						isMove = false;
-
-						break;
-					}
-				}
-
-				if (!isMove) continue;
-			}
-			else // 좌우
-			{
-				// a만큼 하로 가면서 좌우 체크
-				// 하나라도 경계를 만나거나 벽에 막히면 isMove false, break
-				for (int j = 0; j < a; j++)
-				{
-					// 체크 할 위치
-					int checkI = standardPos.first + checkDir[i].first + j;
-					int checkJ = standardPos.second + checkDir[i].second;
-
-					// 경계체크
-					if (checkI < 0 || checkJ < 0 || checkI >= n || checkJ >= m)
-					{
-						isMove = false;
-
-						break;
-					}
-
-					// 장애물체크
-					if (graph[checkI][checkJ] == 1)
-					{
-						isMove = false;
-
-						break;
-					}
-				}
-
-				if (!isMove) continue;
-			}
-
-			// 큐에저장, 거리저장
-			// standard에서 상하좌우 중 하나
-			if (i % 2 == 0)
-			{
-				checkPos.push({ standardPos.first + checkDir[i].first, standardPos.second + checkDir[i].second });
-				dis[standardPos.first + checkDir[i].first][standardPos.second + checkDir[i].second] = dis[standardPos.first][standardPos.second] + 1;
-			}
-			else
-			{
-				if (i == 1)
-				{
-					checkPos.push({ standardPos.first + checkDir[i].first / a, standardPos.second + checkDir[i].second });
-					dis[standardPos.first + checkDir[i].first / a][standardPos.second + checkDir[i].second] = dis[standardPos.first][standardPos.second] + 1;
-				}
-				else if (i == 3)
-				{
-					checkPos.push({ standardPos.first + checkDir[i].first, standardPos.second + checkDir[i].second / b});
-					dis[standardPos.first + checkDir[i].first][standardPos.second + checkDir[i].second / b] = dis[standardPos.first][standardPos.second] + 1;
+					isMove = false;
+					break;
 				}
 			}
+			if (!isMove) continue;
+
+			cp.push({ ci, cj });
+			dis[ci][cj] = dis[si][sj] + 1;
 		}
 	}
+	elp(dis[ePos.lhs][ePos.rhs] - 1);
 
-	//for (auto a : dis)
-	//{
-	//	for (auto b : a)
-	//	{
-	//		cout << b << ' ';
-	//	}
-	//	cout << '\n';
-	//}
-
-	cout << dis[endPos.first - 1][endPos.second - 1] - 1 << '\n';
-
-	return 0;
+	return home;
 }
