b5b589c2df02b5a25840cab026b1af622476e0ce
JungHwi <wnd4114294225@gmail.com>
2025-02-23 15:04:00 +0900

    [Gold III] Title: 채굴, Time: 748 ms, Memory: 6156 KB -BaekjoonHub

--- 백준/Gold/15573. 채굴/채굴.cc
+++ 백준/Gold/15573. 채굴/채굴.cc
@@ -2,110 +2,150 @@
 using namespace std;
 
 #define home 0
+
+#ifdef ONLINE_JUDGE
+#define init ios_base::sync_with_stdio(home); cin.tie(home)
+#else
+#define init ios_base::sync_with_stdio(home); cin.tie(home); ifstream cin("input.txt")
+#endif
+
+#define ll long long
+#define ld long double
+
 #define pii pair<int, int>
-#define loop(v, s, e) for(int v = s; v < e; v++)
-#define elprint(a) cout << a << '\n'
-#define mat(t, v, r, c) vector<vector<t> > v(r, vector<t>(c))
-#define X first
-#define Y second
+#define piii pair<int, pii>
+#define pll pair<ll, ll>
+#define plll pair<ll, pll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define tvec(t, v) vector<t> v
+#define vec(t, v, r) vector<t> v((r))
+#define gmat(t, v, r) vector<vector<t> > v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+#define smat(t, v, r, c, s) vector<vector<vector<t> > > v((r), vector<vector<t>>((c), vector<t>((s))))
+
+#define dir vector<pii> cd = { {-1, home}, {1, home}, { home, -1 }, { home, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+#define kdir vector<pii> kcd = { {-1, -2}, {-2, -1}, { -2, 1 }, { -1, 2 }, { 1, -2 }, { 2, -1 }, { 1, 2 }, { 2, 1 } }
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
+
+int n, m, k;
+queue<pii> cp; dir;
+
+bool Check(int s, vector<vector<int>> &g, vector<vector<bool>> &vis)
+{
+	// 방문체크 초기화
+	loop(i, home, n) loop(j, home, m) vis[i][j] = false;
+
+	// 채굴한 광물 개수
+	int cnt = home;
+
+	// 바닥하고만 닿아있는 광물을 제외한 가장자리 광물부터 멀티 소스 BFS
+	loop(i, home, n)
+	{
+		// 첫 열
+		if (g[i][home] <= s && !vis[i][home])
+		{
+			cp.push({ i, home });
+			vis[i][home] = true;
+			cnt++;
+		}
+
+		// 마지막 열
+		if (g[i][m - 1] <= s && !vis[i][m - 1])
+		{
+			cp.push({ i, m - 1 });
+			vis[i][m - 1] = true;
+			cnt++;
+		}
+	}
+	loop(i, home, m)
+	{
+		// 첫 행
+		if (g[home][i] <= s && !vis[home][i])
+		{
+			cp.push({ home, i });
+			vis[home][i] = true;
+			cnt++;
+		}
+
+		// 마지막 행의 양끝을 제외한 곳은 바닥하고만 닿은 광물이므로 X
+	}
+
+	while (!cp.empty())
+	{
+		int si = cp.front().lhs;
+		int sj = cp.front().rhs;
+		cp.pop();
+
+		loop(i, home, 4)
+		{
+			int ci = si + cd[i].lhs;
+			int cj = sj + cd[i].rhs;
+
+			if (ci < home || cj < home || ci >= n || cj >= m) continue;
+			if (vis[ci][cj]) continue;
+			if (g[ci][cj] > s) continue; // 채굴기 성능보다 강도가 높은 광물은 X
+
+			cp.push({ ci, cj });
+			vis[ci][cj] = true;
+			cnt++;
+		}
+	}
+
+	// k개 이상 채굴하면 가능한 경우
+	return cnt >= k;
+}
 
 // 채굴
 int main()
 {
-	ios_base::sync_with_stdio(home);
-	cin.tie(home);
-	//ifstream cin;
-	//cin.open("input.txt");
+	init;
 
-	int n, m, k;
+	// 바닥하고만 닿아있는 광물을 제외한 가장자리 광물부터 멀티 소스 BFS
+	// 채굴기 성능 D는 강도가 D이하인 광물을 채굴할 수 있음
+	// lo => 0
+	// hi => maxD
+	// s로 채굴했을 때 k개 이상 채굴할 수 있으면 가능한 경우이므로 더 최솟값이 있는지 재탐색
 	cin >> n >> m >> k;
-
-	// 최대 강도
 	mat(int, graph, n, m);
-	int maxStr = 0;
-	loop(i, 0, n) loop(j, 0, m)
+	mat(bool, vis, n, m);
+	int l = home, h = -2147000000;
+	loop(i, home, n) loop(j, home, m)
 	{
 		cin >> graph[i][j];
-		maxStr = max(maxStr, graph[i][j]);
+		h = max(h, graph[i][j]);
 	}
-	mat(bool, vis, n, m);
-	queue<pii> checkPos;
-	vector<pii> checkDir = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
 
-	// 0 => F, maxStr = T
-	// range => 1 ~ maxStr
-	int l = 0, r = maxStr, d = maxStr;
-	while (l <= r)
+	int ans = h; // k개 이상 광물을 캘 수 있는 성능 최솟값
+	while (l <= h)
 	{
-		int standard = (l + r) / 2;
-		bool isStandard = false;
+		int s = (l + h) / 2;
 
-		// 가장자리 => 바닥 광물 X
-		loop(i, 0, n)
+		// s로 채굴했을 때 k개 이상 채굴할 수 있으면 가능한 경우이므로 더 최솟값이 있는지 재탐색
+		if (Check(s, graph, vis))
 		{
-			if (graph[i][0] < standard + 1 && !vis[i][0])
-			{
-				vis[i][0] = true;
-				checkPos.push({ i, 0 });
-			}
-
-			if (graph[i][m - 1] < standard + 1 && !vis[i][m - 1])
-			{
-				vis[i][m - 1] = true;
-				checkPos.push({ i, m - 1 });
-			}
+			ans = s;
+			h = s - 1;
 		}
-		loop(i, 0, m)
-		{
-			if (graph[0][i] < standard + 1 && !vis[0][i])
-			{
-				vis[0][i] = true;
-				checkPos.push({ 0, i });
-			}
-		}
-
-		// BFS
-		int cnt = checkPos.size();
-		while (!checkPos.empty())
-		{
-			pii standardPos = checkPos.front();
-			checkPos.pop();
-
-			// k 이상 채굴
-			if (cnt > k - 1)
-			{
-				isStandard = true;
-				break;
-			}
-
-			loop(i, 0, 4)
-			{
-				int checkI = standardPos.X + checkDir[i].X;
-				int checkJ = standardPos.Y + checkDir[i].Y;
-
-				if (checkI < 0 || checkJ < 0 || checkI >= n || checkJ >= m) continue;
-				if (vis[checkI][checkJ]) continue;
-				if (graph[checkI][checkJ] > standard) continue;
-
-				checkPos.push({ checkI, checkJ });
-				vis[checkI][checkJ] = true;
-				cnt++;
-			}
-		}
-
-		if (isStandard)
-		{
-			d = standard;
-			r = standard - 1;
-		}
-		else l = standard + 1;
-
-		// 초기화
-		loop(i, 0, n) loop(j, 0, m) vis[i][j] = false;
-		while (!checkPos.empty()) checkPos.pop();
+		else l = s + 1;
 	}
-
-	elprint(d);
+	elp(ans);
 
 	return home;
 }
