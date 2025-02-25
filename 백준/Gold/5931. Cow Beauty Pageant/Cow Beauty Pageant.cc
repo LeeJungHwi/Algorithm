4a3bee6029bd597948796c71c9a07f271baf3494
JungHwi <wnd4114294225@gmail.com>
2025-02-25 20:17:49 +0900

    [Gold V] Title: Cow Beauty Pageant, Time: 0 ms, Memory: 2164 KB -BaekjoonHub

--- 백준/Gold/5931. Cow Beauty Pageant/Cow Beauty Pageant.cc
+++ 백준/Gold/5931. Cow Beauty Pageant/Cow Beauty Pageant.cc
@@ -1,203 +1,150 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <string>
-#include <map>
-#include <fstream>
+#include <bits/stdc++.h>
 using namespace std;
+
+#define home 0
+
+#ifdef ONLINE_JUDGE
+#define init ios_base::sync_with_stdio(home); cin.tie(home)
+#else
+#define init ios_base::sync_with_stdio(home); cin.tie(home); ifstream cin("input.txt")
+#endif
+
+#define ll long long
+#define ld long double
+
+#define pii pair<int, int>
+#define piii pair<int, pii>
+#define pll pair<ll, ll>
+#define plll pair<ll, pll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define tvec(t, v) vector<t> v
+#define vec(t, v, r) vector<t> v((r))
+#define ivec(t, v, r, i) vector<t> v((r), i)
+#define gmat(t, v, r) vector<vector<t> > v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+#define imat(t, v, r, c, i) vector<vector<t> > v((r), vector<t>((c), i))
+#define smat(t, v, r, c, s) vector<vector<vector<t> > > v((r), vector<vector<t>>((c), vector<t>((s))))
+#define ismat(t, v, r, c, s, i) vector<vector<vector<t> > > v((r), vector<vector<t>>((c), vector<t>((s), i)))
+
+#define dir vector<pii> cd = { {-1, home}, {1, home}, { home, -1 }, { home, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+#define kdir vector<pii> kcd = { {-1, -2}, {-2, -1}, { -2, 1 }, { -1, 2 }, { 1, -2 }, { 2, -1 }, { 1, 2 }, { 2, 1 } }
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
 
 // Cow Beauty Pageant
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	//ifstream cin;
-	//cin.open("input.txt");
+	init;
 
-	int n, m; // N, M 6, 16
-	cin >> n >> m;
-
-	vector<vector<int> > graph(n, vector<int>(m)); // 그래프
-	vector<vector<int> > dis(n, vector<int>(m)); // 거리
-	vector<vector<pair<int, int> > > labelPos; // 각 반점 내부좌표 저장
-	queue<pair<int, int> > checkPos; // 체크 할 위치
-	vector<pair<int, int> > checkDir; // 상하좌우
-	checkDir.push_back({ -1, 0 });
-	checkDir.push_back({ 1, 0 });
-	checkDir.push_back({ 0, -1 });
-	checkDir.push_back({ 0, 1 });
-
-	// 그래프 초기화
-	// . => 0 X => 1로 치환
-	string inputString;
-	for (int i = 0; i < n; i++)
+	// 영역 라벨링 후 (라벨, 위치) 맵핑
+	// 맵 돌면서 현재 라벨 보다 큰 라벨과 만나는 최단거리
+	// 아스키코드는 라벨이 127을 넘으면 오버플로우가 발생하므로 int형 그래프로 치환
+	int n, m; cin >> n >> m;
+	mat(int, graph, n, m);
+	mat(int, dis, n, m);
+	queue<pii> cp; dir;
+	tvec(pii, sPos);
+	char ic;
+	loop(i, home, n) loop(j, home, m)
 	{
-		cin >> inputString;
-		for (int j = 0; j < m; j++)
+		cin >> ic;
+
+		// 아스키코드는 라벨이 127을 넘으면 오버플로우가 발생하므로 int형 그래프로 치환 => .(-1) X(0)
+		if (ic == '.') graph[i][j] = -1;
+		if (ic == 'X')
 		{
-			if (inputString[j] == '.') graph[i][j] = 0;
-			else if (inputString[j] == 'X') graph[i][j] = 1;
+			graph[i][j] = home;
+			sPos.push_back({ i, j });
 		}
 	}
 
-	// 1.그래프 돌면서 각 반점 라벨링  : 첫번째 반점은 2 두번째 반점은 3
-	// 2.그래프 돌면서 각 반점 내부 1 좌표를 큐에 넣고
-	// 각 반점 라벨보다 큰 라벨을 갖는 다른 반점을 연결하는 최단거리 갱신
-
-	// 1.그래프 돌면서 각 반점 라벨링  : 첫번째 반점은 2 두번째 반점은 3
-
-	int curLabel = 1; // 현재 라벨
-
-	for (int i = 0; i < n; i++) // 그래프 돌면서
+	// 영역 라벨링 후 (라벨, 위치) 맵핑
+	int curLabel = home;
+	map<int, vector<pii>> labelMap;
+	loop(i, home, sPos.size()) if (graph[sPos[i].lhs][sPos[i].rhs] == home)
 	{
-		for (int j = 0; j < m; j++)
+		graph[sPos[i].lhs][sPos[i].rhs] = ++curLabel;
+		cp.push(sPos[i]);
+		labelMap[curLabel].push_back(sPos[i]);
+
+		while (!cp.empty())
 		{
-			if (graph[i][j] == 1) // 아직 라벨링 되지않은곳을 만나면
+			int si = cp.front().lhs;
+			int sj = cp.front().rhs;
+			cp.pop();
+
+			loop(i, home, 4)
 			{
-				// 이 위치부터 BFS
-				checkPos.push({ i, j });
-				curLabel++;
-				graph[i][j] = curLabel;
-				vector<pair<int, int> > curLabelPos;
-				curLabelPos.push_back({ i, j });
+				int ci = si + cd[i].lhs;
+				int cj = sj + cd[i].rhs;
 
-				while (!checkPos.empty()) // 큐가 빌때까지
-				{
-					// 기준위치 꺼냄
-					pair<int, int> standardPos = checkPos.front();
-					checkPos.pop();
+				if (ci < home || cj < home || ci >= n || cj >= m) continue;
+				if (graph[ci][cj] > home) continue; // 이미 라벨링된 영역은 X
+				if (graph[ci][cj] == -1) continue;
 
-					// 상하좌우
-					for (int k = 0; k < 4; k++)
-					{
-						// 체크 할 위치
-						int checkI = standardPos.first + checkDir[k].first;
-						int checkJ = standardPos.second + checkDir[k].second;
-
-						// 경계체크
-						if (checkI < 0 || checkJ < 0 || checkI >= n || checkJ >= m) continue;
-
-						// 방문체크(라벨로)
-						if (graph[checkI][checkJ] == curLabel) continue;
-
-						// 벽체크
-						if (graph[checkI][checkJ] == 0) continue;
-
-						// 큐에저장 라벨링 내부좌표백업
-						checkPos.push({ checkI, checkJ });
-						graph[checkI][checkJ] = curLabel;
-						curLabelPos.push_back({ checkI, checkJ });
-					}
-				}
-
-				// 현재 반점의 내부좌표 저장
-				labelPos.push_back(curLabelPos);
+				graph[ci][cj] = curLabel;
+				cp.push({ ci, cj });
+				labelMap[curLabel].push_back({ ci, cj });
 			}
 		}
 	}
 
-	//for (auto a : graph)
-	//{
-	//	for (auto b : a)
-	//	{
-	//		cout << b << ' ';
-	//	}
-	//	cout << '\n';
-	//}
-
-	//for (auto a : labelPos)
-	//{
-	//	for (auto b : a)
-	//	{
-	//		cout << b.first << ' ' << b.second << '\n';
-	//	}
-	//	cout << '\n';
-	//}
-
-	// 2.그래프 돌면서 각 반점 내부 1 좌표를 큐에 넣고
-	// 각 반점 라벨보다 큰 라벨을 갖는 다른 반점을 연결하는 최단거리 갱신
-
-	curLabel = 2; // 첫 반점 부터
-	int minDis = 2147000000; // 두 반점을 잇는 최단거리
-
-	for (int i = 0; i < n; i++) // 그래프 돌면서
+	// 맵 돌면서 현재 라벨 보다 큰 라벨과 만나는 최단거리
+	mloop(it, labelMap)
 	{
-		for (int j = 0; j < m; j++)
+		// 현재 라벨의 모든 위치 부터 멀티 소스 BFS
+		loop(i, home, it->rhs.size())
 		{
-			if (graph[i][j] == curLabel) // 각 반점을 만나면
-			{
-				// 일단 거리 및 큐 초기화
-				for (int k = 0; k < n; k++)
-				{
-					for (int l = 0; l < m; l++)
-					{
-						dis[k][l] = 0;
-					}
-				}
-				while (!checkPos.empty()) checkPos.pop();
-
-				// 각 반점의 내부좌표부터 BFS 돌리다가
-				// 자기보다 라벨이 큰 반점을 만나면 최단거리 갱신
-
-				// 각 반점의 내부좌표 큐에 저장하고 거리 1
-				for (int k = 0; k < labelPos[curLabel - 2].size(); k++)
-				{
-					checkPos.push({ labelPos[curLabel - 2][k] });
-					dis[labelPos[curLabel - 2][k].first][labelPos[curLabel - 2][k].second] = 1;
-				}
-
-				// 각 반점의 내부좌표부터 BFS
-				while (!checkPos.empty()) // 큐가 빌때까지
-				{
-					// 기준위치 꺼냄
-					pair<int, int> standardPos = checkPos.front();
-					checkPos.pop();
-
-					bool isDot = false; // 다른 반점을 찾았는지 체크
-
-					// 상하좌우
-					for (int k = 0; k < 4; k++)
-					{
-						// 체크 할 위치
-						int checkI = standardPos.first + checkDir[k].first;
-						int checkJ = standardPos.second + checkDir[k].second;
-
-						// 경계체크
-						if (checkI < 0 || checkJ < 0 || checkI >= n || checkJ >= m) continue;
-
-						// 자기보다 큰 라벨을 만나면 최단거리 갱신
-						if (graph[checkI][checkJ] > curLabel)
-						{
-							isDot = true;
-							minDis = min(minDis, dis[standardPos.first][standardPos.second]);
-
-							break;
-						}
-
-						// 같은라벨체크
-						if (graph[checkI][checkJ] == curLabel) continue;
-
-						// 방문체크(거리로)
-						if (dis[checkI][checkJ] > 0) continue;
-
-						// 큐에저장 거리저장
-						checkPos.push({ checkI, checkJ });
-						dis[checkI][checkJ] = dis[standardPos.first][standardPos.second] + 1;
-					}
-
-					// 다른 반점을 찾았으면 break
-					if (isDot) break;
-				}
-
-				// 다음 반점도 다음 반점 보다 큰 라벨을 반점 체크하기 위해
-				curLabel++;
-			}
+			cp.push(it->rhs[i]);
+			dis[it->rhs[i].lhs][it->rhs[i].rhs] = 1;
 		}
+
+		int ans = MAX; // 현재 라벨 보다 큰 라벨과 만나는 최단거리
+		while (!cp.empty())
+		{
+			int si = cp.front().lhs;
+			int sj = cp.front().rhs;
+			cp.pop();
+
+			loop(i, home, 4)
+			{
+				int ci = si + cd[i].lhs;
+				int cj = sj + cd[i].rhs;
+
+				if (ci < home || cj < home || ci >= n || cj >= m) continue;
+				if (dis[ci][cj] > home) continue;
+				// 현재 라벨 보다 큰 라벨을 만나면 최단거리
+				if (graph[ci][cj] > it->lhs) { ans = dis[si][sj] - 1; break; }
+				if (graph[ci][cj] != -1) continue;
+
+				cp.push({ ci, cj });
+				dis[ci][cj] = dis[si][sj] + 1;
+			}
+
+			// 최단거리 구했으면 종료
+			if (ans != MAX) break;
+		}
+
+		// 최단거리 구했으면 출력 후 거리 초기화
+		if (ans != MAX) elp(ans);
+		loop(i, home, n) loop(j, home, m) dis[i][j] = home;
 	}
 
-	// 1로 시작했으니 1 빼줌
-	cout << minDis - 1 << '\n';
-
-	return 0;
+	return home;
 }
