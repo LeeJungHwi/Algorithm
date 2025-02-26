e6bfadfa0db6bb1fa34b9e0d7ee739ab8dc5b06c
JungHwi <wnd4114294225@gmail.com>
2025-02-27 06:59:36 +0900

    [Gold III] Title: 로봇, Time: 0 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Gold/1726. 로봇/로봇.cc
+++ 백준/Gold/1726. 로봇/로봇.cc
@@ -1,169 +1,151 @@
-#include <iostream>
-#include <queue>
-#include <cstring>
-#include <vector>
-#include <algorithm>
-#include <fstream>
+#include <bits/stdc++.h>
 using namespace std;
 
-// 기준위치 구조체
-struct Standard
-{
-	// 행, 열, 방향, 이동 및 회전 횟수
-	int col;
-	int row;
-	int dir;
-	int transformCnt;
+#define home 0
 
-	// 생성자
-	Standard() {}
-	Standard(int c, int r, int d, int t) : col(c), row(r), dir(d), transformCnt(t) {}
+#ifdef ONLINE_JUDGE
+#define init ios_base::sync_with_stdio(home); cin.tie(home)
+#else
+#define init ios_base::sync_with_stdio(home); cin.tie(home); ifstream cin("input.txt")
+#endif
 
-	// 횟수 가 적은 것이 먼저
-	bool operator<(const Standard& standard) const
-	{
-		return transformCnt > standard.transformCnt;
-	}
-};
+#define ll long long
+#define ld long double
+
+#define pii pair<int, int>
+#define piii pair<int, pii>
+#define pll pair<ll, ll>
+#define plll pair<ll, pll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define tvec(t, v) vector<t> v
+#define vec(t, v, r) vector<t> v((r))
+#define ivec(t, v, r, i) vector<t> v((r), i)
+#define gmat(t, v, r) vector<vector<t> > v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+#define imat(t, v, r, c, i) vector<vector<t> > v((r), vector<t>((c), i))
+#define smat(t, v, r, c, s) vector<vector<vector<t> > > v((r), vector<vector<t>>((c), vector<t>((s))))
+#define ismat(t, v, r, c, s, i) vector<vector<vector<t> > > v((r), vector<vector<t>>((c), vector<t>((s), i)))
+
+#define dir vector<pii> cd = { {-1, home}, {home, 1}, { home, -1 }, { 1, home }, { -1, home }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+#define kdir vector<pii> kcd = { {-1, -2}, {-2, -1}, { -2, 1 }, { -1, 2 }, { 1, -2 }, { 2, -1 }, { 1, 2 }, { 2, 1 } }
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
 
 // 로봇
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	//ifstream cin;
-	//cin.open("input.txt");
+	init;
 
-	int m, n; // M, N 5, 6
-	cin >> m >> n;
+	// 시작 위치와 방향에서 끝 위치와 방향이 되도록하는 명령 최소 횟수
+	// 명령 1 => 1, 2, 3칸 이동
+	// 명령 2 => 왼쪽, 오른쪽 회전
+	// 방향 => 1(우) 2(좌) 3(하) 4(상)
+	
+	int n, m; cin >> n >> m;
+	mat(int, graph, n, m);
+	smat(int, dis, n, m, 5); // vis[i][j][k] => i,j로 k방향에서 왔을 때 명령 최소 횟수
+	queue<pair<pii, pii>> cp; dir; // (위치, 방향, 명령 횟수)
+	loop(i, home, n) loop(j, home, m) cin >> graph[i][j];
 
-	vector<vector<int>> graph(m + 1, vector<int>(n + 1)); // 그래프
-	vector<vector<vector<int>>> dis(m + 1, vector<vector<int>>(n + 1, vector<int>(5))); // 거리
-	priority_queue<Standard> checkPos; // 체크 할 위치
-	vector<pair<int, int>> checkDir; // 동서남북
-	checkDir.push_back({ 0, 1 });
-	checkDir.push_back({ 0, -1 });
-	checkDir.push_back({ 1, 0 });
-	checkDir.push_back({ -1, 0 });
+	// 시작 위치 및 방향, 끝 위치 및 방향 입력
+	pii sPos, ePos;
+	int sDir, eDir;
+	cin >> sPos.lhs >> sPos.rhs >> sDir; sPos.lhs--; sPos.rhs--;
+	cin >> ePos.lhs >> ePos.rhs >> eDir; ePos.lhs--; ePos.rhs--;
 
-	// 0 0 0 0 0 0
-	// 0 1 1 0 1 0
-	// 0 1 0 0 0 0
-	// 0 0 1 1 1 0
-	// 1 0 0 0 0 0
-	for (int i = 1; i < m + 1; i++)
+	// 시작 위치 및 방향 부터 BFS
+	cp.push({ sPos, {sDir, home} });
+	dis[sPos.lhs][sPos.rhs][sDir] = 1;
+
+	while (!cp.empty())
 	{
-		for (int j = 1; j < n + 1; j++)
+		int si = cp.front().lhs.lhs;
+		int sj = cp.front().lhs.rhs;
+		int sd = cp.front().rhs.lhs;
+		int sc = cp.front().rhs.rhs;
+		cp.pop();
+
+		// 끝 위치 및 방향에 도착하면 종료
+		if (si == ePos.lhs && sj == ePos.rhs && sd == eDir)
 		{
-			cin >> graph[i][j];
+			elp(sc);
+			return home;
+		}
+
+		// 회전하지 않고 현재 방향 그대로 1, 2, 3칸 진행하는 경우
+		loop(i, 1, 4)
+		{
+			int ci = si + cd[sd].lhs * i;
+			int cj = sj + cd[sd].rhs * i;
+			int cc = sc + 1;
+
+			if (ci < home || cj < home || ci >= n || cj >= m) break;
+			if (graph[ci][cj] == 1) break;
+			// 기존 명령 횟수 보다 더 많은 명령으로 왔다면 X
+			if (dis[ci][cj][sd] > home && dis[ci][cj][sd] <= cc) continue;
+
+			cp.push({ {ci, cj}, {sd, cc} });
+			dis[ci][cj][sd] = cc;
+		}
+
+		// 회전하는 경우
+		
+		// 1.왼쪽으로 회전
+		// 1(우) => 4(상)
+		// 2(좌) => 3(하)
+		// 3(하) => 1(우)
+		// 4(상) => 2(좌)
+		int cdir = sd;
+		switch (cdir)
+		{
+			case 1: cdir = 4; break;
+			case 2: cdir = 3; break;
+			case 3: cdir = 1; break;
+			case 4: cdir = 2; break;
+		}
+
+		if (dis[si][sj][cdir] == home)
+		{
+			cp.push({ {si, sj}, {cdir, sc + 1} });
+			dis[si][sj][cdir] = sc + 1;
+		}
+
+		// 2.오른쪽으로 회전
+		// 1(우) => 3(하)
+		// 2(좌) => 4(상)
+		// 3(하) => 2(좌)
+		// 4(상) => 1(우)
+		cdir = sd;
+		switch (cdir)
+		{
+			case 1: cdir = 3; break;
+			case 2: cdir = 4; break;
+			case 3: cdir = 2; break;
+			case 4: cdir = 1; break;
+		}
+
+		if (dis[si][sj][cdir] == home)
+		{
+			cp.push({ {si, sj}, {cdir, sc + 1} });
+			dis[si][sj][cdir] = sc + 1;
 		}
 	}
 
-	// 4 2 3
-	// 2 4 1
-	int col, row, dir; // 행, 열, 방향
-
-	cin >> col >> row >> dir;
-	Standard startPos = Standard(col, row, dir, 0); // 시작위치
-
-	cin >> col >> row >> dir;
-	Standard endPos = Standard(col, row, dir, 0); // 도착위치
-
-	// 시작 위치부터 BFS 돌리기
-	checkPos.push(startPos);
-	dis[startPos.col][startPos.row][startPos.dir] = startPos.transformCnt;
-
-	while (!checkPos.empty()) // 큐가 빌때까지
-	{
-		// 기준위치 꺼냄
-		Standard standardPos = checkPos.top();
-		checkPos.pop();
-
-		// 종료조건: 목표 지점 및 방향에 도달한 경우
-		if (standardPos.col == endPos.col && standardPos.row == endPos.row && standardPos.dir == endPos.dir)
-		{
-			cout << standardPos.transformCnt << '\n';
-
-			return 0;
-		}
-
-		// 이동횟수 처리
-		// Go K : 1, 2, 3 칸 앞까지 이동하는지 체크
-		// 누적해서 이동횟수 계산
-		for (int i = 1; i < 4; i++)
-		{
-			int c = standardPos.col + checkDir[standardPos.dir - 1].first * i;
-			int r = standardPos.row + checkDir[standardPos.dir - 1].second * i;
-			int d = standardPos.dir;
-			int t = standardPos.transformCnt + 1;
-
-			// 경계 체크
-			if (c < 1 || r < 1 || c >= m + 1 || r >= n + 1)
-			{
-				break;
-			}
-
-			// 궤도 체크
-			if (graph[c][r] == 1)
-			{
-				break;
-			}
-
-			// 이미 할당되어있는 거리값이 더 비용이 적은 경우인지 체크
-			if (dis[c][r][d] > 0 && dis[c][r][d] <= t)
-			{
-				continue;
-			}
-
-			// 큐에 추가 후 방문 체크
-			checkPos.push(Standard(c, r, d, t));
-			dis[c][r][d] = t;
-		}
-
-		// 회전횟수 처리
-		// 기준위치에서 회전할수있는 경우 체크
-		// 회전할때 회전횟수 계산
-		for (int i = 1; i < 5; i++)
-		{
-			// 같은방향체크
-			if (standardPos.dir == i)
-			{
-				continue;
-			}
-
-			// 회전횟수계산
-			// 방향이 90도인 경우 1
-			int turnCnt = 1;
-
-			// 방향이 180도인 경우 2
-			if (standardPos.dir == 1 && i == 2) // 동 서
-			{
-				turnCnt++;
-			}
-			else if (standardPos.dir == 2 && i == 1) // 서 동
-			{
-				turnCnt++;
-			}
-			else if (standardPos.dir == 3 && i == 4) // 남 북
-			{
-				turnCnt++;
-			}
-			else if (standardPos.dir == 4 && i == 3) // 북 남
-			{
-				turnCnt++;
-			}
-
-			// 이미 할당되어있는 거리값이 더 비용이 적은 경우인지 체크
-			if (dis[standardPos.col][standardPos.row][i] > 0 && dis[standardPos.col][standardPos.row][i] <= standardPos.transformCnt + turnCnt)
-			{
-				continue;
-			}
-
-			// 큐에 추가 후 방문 체크
-			checkPos.push(Standard(standardPos.col, standardPos.row, i, standardPos.transformCnt + turnCnt));
-			dis[standardPos.col][standardPos.row][i] = standardPos.transformCnt + turnCnt;
-		}
-	}
-
-	return 0;
+	return home;
 }
