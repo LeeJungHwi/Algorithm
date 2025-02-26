32338606a9a2e68f96880025ca091b5a9ad3ef5c
JungHwi <wnd4114294225@gmail.com>
2025-02-27 02:57:13 +0900

    [Gold III] Title: 시루의 백화점 구경, Time: 336 ms, Memory: 66964 KB -BaekjoonHub

--- 백준/Gold/25307. 시루의 백화점 구경/시루의 백화점 구경.cc
+++ 백준/Gold/25307. 시루의 백화점 구경/시루의 백화점 구경.cc
@@ -1,142 +1,137 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <string>
-#include <map>
-#include <fstream>
+#include <bits/stdc++.h>
 using namespace std;
+
+#define home 0
+
+#ifdef ONLINE_JUDGE
+#define init ios_base::sync_with_stdio(home); cin.tie(home)
+#else
+#define init ios_base::sync_with_stdio(home); cin.tie(home); ifstream cin("input.txt")
+#endif
+
+#define ll long long
+#define ld long double
+
+#define pii pair<int, int>
+#define piii pair<int, pii>
+#define pll pair<ll, ll>
+#define plll pair<ll, pll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define tvec(t, v) vector<t> v
+#define vec(t, v, r) vector<t> v((r))
+#define ivec(t, v, r, i) vector<t> v((r), i)
+#define gmat(t, v, r) vector<vector<t> > v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+#define imat(t, v, r, c, i) vector<vector<t> > v((r), vector<t>((c), i))
+#define smat(t, v, r, c, s) vector<vector<vector<t> > > v((r), vector<vector<t>>((c), vector<t>((s))))
+#define ismat(t, v, r, c, s, i) vector<vector<vector<t> > > v((r), vector<vector<t>>((c), vector<t>((s), i)))
+
+#define dir vector<pii> cd = { {-1, home}, {1, home}, { home, -1 }, { home, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+#define kdir vector<pii> kcd = { {-1, -2}, {-2, -1}, { -2, 1 }, { -1, 2 }, { 1, -2 }, { 2, -1 }, { 1, 2 }, { 2, 1 } }
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
 
 // 시루의 백화점 구경
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	//ifstream cin;
-	//cin.open("input.txt");
+	init;
 
-	// 시작 위치 저장
-	// 마네킹 위치 큐에 저장
-	// 확장하면서 의자 cnt--
-	// 확장 할 때 거리 갱신된 곳 중 최단거리는 큐에 다시 저장
-	// 방문한 곳은 큐에 중복 저장 X
-	// 의자 cnt == 0이면 -1
-	// BFS 돌리면서 의자 까지 거리
-	// 거리 갱신 안 되면 -1
+	// 모든 마네킹 위치(3)를큐에 넣고 멀티 소스 BFS => 거리 k까지만
+	// 시루 위치(4) 부터 BFS 돌려서 의자(2)를 만나면 거리 출력
+	// 의자를 만나지 못 했으면 -1
 
-	int n, m, k;
-	cin >> n >> m >> k;
-	vector<vector<int> > graph(n, vector<int>(m));
-	vector<vector<int> > dis(n, vector<int>(m));
-	vector<vector<bool> > vis(n, vector<bool>(m));
-	queue<pair<int, int> > checkPos;
-	vector<pair<int, int> > checkDir;
-	checkDir.push_back({ -1, 0 });
-	checkDir.push_back({ 1, 0 });
-	checkDir.push_back({ 0, -1 });
-	checkDir.push_back({ 0, 1 });
-
-	pair<int, int> startPos;
-	int chairCnt = 0;
-	vector<pair<int, int> > mannequinPos;
-	for (int i = 0; i < n; i++)
+	int n, m, k; cin >> n >> m >> k;
+	mat(int, graph, n, m);
+	mat(int, dis, n, m);
+	queue<pii> cp; dir;
+	pii sPos;
+	loop(i, home, n) loop(j, home, m)
 	{
-		for (int j = 0; j < m; j++)
+		cin >> graph[i][j];
+		
+		// 마네킹 위치들 큐에 넣기
+		if (graph[i][j] == 3)
 		{
-			cin >> graph[i][j];
-			if (graph[i][j] == 4) startPos = { i, j };
-			else if (graph[i][j] == 3)
-			{
-				checkPos.push({ i, j });
-				dis[i][j] = 1;
-			}
-			else if (graph[i][j] == 2) chairCnt++;
+			cp.push({ i, j });
+			dis[i][j] = 1;
 		}
+		// 시루 위치 저장
+		else if (graph[i][j] == 4) sPos = { i, j };
 	}
 
-	while (!checkPos.empty())
+	// 모든 마네킹 위치(3)를큐에 넣고 멀티 소스 BFS => 거리 k까지만
+	while (!cp.empty())
 	{
-		pair<int, int> standardPos = checkPos.front();
-		checkPos.pop();
+		int si = cp.front().lhs;
+		int sj = cp.front().rhs;
+		cp.pop();
 
-		// k번 확장
-		if (dis[standardPos.first][standardPos.second] > k) continue;
+		// 마네킹에서 거리 k까지만 확장
+		if (dis[si][sj] > k) continue;
 
-		for (int j = 0; j < 4; j++)
+		loop(i, home, 4)
 		{
-			int checkI = standardPos.first + checkDir[j].first;
-			int checkJ = standardPos.second + checkDir[j].second;
+			int ci = si + cd[i].lhs;
+			int cj = sj + cd[i].rhs;
 
-			if (checkI < 0 || checkJ < 0 || checkI >= n || checkJ >= m) continue;
+			if (ci < home || cj < home || ci >= n || cj >= m) continue;
+			if (dis[ci][cj] > home) continue;
+			// 마네킹 거리는 벽 검사 X
 
-			// 거리가 체크된 곳 중 최단거리만 체크
-			if (dis[checkI][checkJ] != 0 && dis[checkI][checkJ] < dis[standardPos.first][standardPos.second] + 1) continue;
-
-			// 큐에 중복 저장 방지 => 메모리 초과
-			if (vis[checkI][checkJ] > 0) continue;
-
-			checkPos.push({ checkI, checkJ });
-			dis[checkI][checkJ] = dis[standardPos.first][standardPos.second] + 1;
-			vis[checkI][checkJ] = true;
-
-			// 의자면 의자 수 감소하고 3으로
-			if (graph[checkI][checkJ] == 2)
-			{
-				chairCnt--;
-				graph[checkI][checkJ] = 3;
-			}
-			else if (graph[checkI][checkJ] < 2) graph[checkI][checkJ] = 3;
+			cp.push({ ci, cj });
+			dis[ci][cj] = dis[si][sj] + 1;
 		}
 	}
+	
+	// 시루 위치(4) 부터 BFS 돌려서 의자(2)를 만나면 거리 출력
+	// 의자를 만나지 못 했으면 -1
+	cp.push(sPos);
+	dis[sPos.lhs][sPos.rhs] = 1;
 
-	// 의자 수가 없으면 -1
-	if (chairCnt == 0)
+	while (!cp.empty())
 	{
-		cout << -1 << '\n';
-		return 0;
-	}
+		int si = cp.front().lhs;
+		int sj = cp.front().rhs;
+		cp.pop();
 
-	//for (auto a : graph)
-	//{
-	//	for (auto b : a)
-	//	{
-	//		cout << b << ' ';
-	//	}
-	//	cout << '\n';
-	//}
-
-	// 시작위치 부터 의자위치 까지 거리
-	checkPos.push(startPos);
-	dis[startPos.first][startPos.second] = 1;
-
-	while (!checkPos.empty())
-	{
-		pair<int, int> standardPos = checkPos.front();
-		checkPos.pop();
-
-		// 의자 찾으면
-		if (graph[standardPos.first][standardPos.second] == 2)
+		// 의자(2)를 만나면 거리 출력
+		if (graph[si][sj] == 2)
 		{
-			cout << dis[standardPos.first][standardPos.second] - 1 << '\n';
-			return 0;
+			elp(dis[si][sj] - 1);
+			return home;
 		}
 
-		for (int i = 0; i < 4; i++)
+		loop(i, home, 4)
 		{
-			int checkI = standardPos.first + checkDir[i].first;
-			int checkJ = standardPos.second + checkDir[i].second;
+			int ci = si + cd[i].lhs;
+			int cj = sj + cd[i].rhs;
 
-			if (checkI < 0 || checkJ < 0 || checkI >= n || checkJ >= m) continue;
-			if (dis[checkI][checkJ] > 0) continue;
-			if (graph[checkI][checkJ] != 0 && graph[checkI][checkJ] != 2) continue;
+			if (ci < home || cj < home || ci >= n || cj >= m) continue;
+			if (dis[ci][cj] > home) continue;
+			if (graph[ci][cj] == 1) continue;
 
-			checkPos.push({ checkI, checkJ });
-			dis[checkI][checkJ] = dis[standardPos.first][standardPos.second] + 1;
+			cp.push({ ci, cj });
+			dis[ci][cj] = dis[si][sj] + 1;
 		}
 	}
+	// 의자를 만나지 못 했으면 -1
+	elp(-1);
 
-	// 여기 오면 의자 못 찾음
-	cout << -1 << '\n';
-
-	return 0;
+	return home;
 }
