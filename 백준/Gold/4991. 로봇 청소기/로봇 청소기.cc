c0ebb74925c4b2ace3df63167dbe1f2285ef67f3
JungHwi <wnd4114294225@gmail.com>
2025-03-02 05:45:06 +0900

    [Gold I] Title: 로봇 청소기, Time: 40 ms, Memory: 2944 KB -BaekjoonHub

--- 백준/Gold/4991. 로봇 청소기/로봇 청소기.cc
+++ 백준/Gold/4991. 로봇 청소기/로봇 청소기.cc
@@ -3,27 +3,44 @@ using namespace std;
 
 #define home 0
 
+#ifdef ONLINE_JUDGE
+#define init ios_base::sync_with_stdio(home); cin.tie(home)
+#else
+#define init ios_base::sync_with_stdio(home); cin.tie(home); ifstream cin("input.txt")
+#endif
+
 #define ll long long
 #define ld long double
 
 #define pii pair<int, int>
+#define piii pair<int, pii>
 #define pll pair<ll, ll>
+#define plll pair<ll, pll>
 
 #define loop(v, s, e) for(int v = (s); v < (e); v++)
 #define rloop(v, s, e) for(int v = (s); v > (e); v--)
 #define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
 #define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
-#define aloop(t, v, s, c, e, i) for(t v = (s); v c (e); i)
 
 #define p(a) cout << (a)
 #define elp(a) cout << (a) << '\n'
 #define scp(a) cout << (a) << ' '
 
+#define tvec(t, v) vector<t> v
 #define vec(t, v, r) vector<t> v((r))
-#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+#define ivec(t, v, r, i) vector<t> v((r), i)
 #define gmat(t, v, r) vector<vector<t> > v((r))
-#define dir vector<pii> cd = { {-1, 0}, {1, 0}, { 0, -1 }, { 0, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+#define imat(t, v, r, c, i) vector<vector<t> > v((r), vector<t>((c), i))
+#define smat(t, v, r, c, s) vector<vector<vector<t> > > v((r), vector<vector<t>>((c), vector<t>((s))))
+#define ismat(t, v, r, c, s, i) vector<vector<vector<t> > > v((r), vector<vector<t>>((c), vector<t>((s), i)))
+#define ssmat(t, v, r, c, s1, s2) vector<vector<vector<vector<t> > > > v((r), vector<vector<vector<t>>>((c), vector<vector<t>>((s1), vector<t>((s2)))))
+#define issmat(t, v, r, c, s1, s2, i) vector<vector<vector<vector<t> > > > v((r), vector<vector<vector<t>>>((c), vector<vector<t>>((s1), vector<t>((s2), i))))
+#define sssmat(t, v, r, c, s1, s2, s3) vector<vector<vector<vector<vector<t> > > > > v((r), vector<vector<vector<vector<t>>>>((c), vector<vector<vector<t>>>((s1), vector<vector<t>>((s2), vector<t>((s3))))))
+#define isssmat(t, v, r, c, s1, s2, s3, i) vector<vector<vector<vector<vector<t> > > > > v((r), vector<vector<vector<vector<t>>>>((c), vector<vector<vector<t>>>((s1), vector<vector<t>>((s2), vector<t>((s3), i)))))
 
+#define dir vector<pii> cd = { {-1, home}, {1, home}, { home, -1 }, { home, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+#define kdir vector<pii> kcd = { {-1, -2}, {-2, -1}, { -2, 1 }, { -1, 2 }, { 1, -2 }, { 2, -1 }, { 1, 2 }, { 2, 1 } }
 #define lhs first
 #define rhs second
 
@@ -34,144 +51,91 @@ using namespace std;
 const int MAX = 2147000000;
 const int MIN = -2147000000;
 
-vector<pii> dirtyPos;
-vec(bool, vis, 10);
-map<pii, int> dMap;
-int moveCnt, cleanCnt;
-int ans = MAX;
-
-void DFS(int L)
-{
-	if (cleanCnt == dirtyPos.size()) { ans = min(ans, moveCnt); return; }
-
-	loop(i, 1, dirtyPos.size() + 1)
-	{
-		if (vis[i - 1]) continue;
-		if (!dMap[{L, i}]) continue;
-
-		vis[i - 1] = true;
-		moveCnt += dMap[{L, i}];
-		cleanCnt += 1;
-		DFS(i);
-		cleanCnt -= 1;
-		moveCnt -= dMap[{L, i}];
-		vis[i - 1] = false;
-	}
-}
-
 // 로봇 청소기
 int main()
 {
-	ios_base::sync_with_stdio(home);
-	cin.tie(home);
-	#ifndef ONLINE_JUDGE
-	ifstream cin("input.txt");
-	#endif
+	init;
+
+	// 더러운 칸 조합 상태 => 1 << 더러운 칸 개수
 
 	dir;
 	while (true)
 	{
-		int n, m;
-		cin >> m >> n;
-		if (m == home && n == home) break;
+		int n, m; cin >> m >> n;
+		if (n == home && m == home) break;
+
 		mat(char, graph, n, m);
-		mat(int, dis, n, m);
-		queue<pii> cp;
+		queue<piii> cp; // (더러운 칸 조합 상태, 위치)
+		pii sPos;
+		tvec(pii, dirtyPos);
 		loop(i, home, n) loop(j, home, m)
 		{
 			cin >> graph[i][j];
-			if (graph[i][j] == 'o') { cp.push({ i, j }); dis[i][j] = 1; }
+
+			// 로봇 청소기 위치와 더러운 칸 위치 저장
+			if (graph[i][j] == 'o') sPos = { i, j };
 			else if (graph[i][j] == '*') dirtyPos.push_back({ i, j });
 		}
+		ismat(int, dis, n, m, 1 << dirtyPos.size(), MAX); // dis[i][j][k] => i,j에서 더러운 칸 조합 상태가 k일 때 최단거리
 
-		// 시작 칸 <-> 더러운 칸 메모
+		cp.push({ home, sPos });
+		dis[sPos.lhs][sPos.rhs][home] = 1;
+
+		bool isClean = false;
 		while (!cp.empty())
 		{
-			pii s = cp.front(); cp.pop();
+			int si = cp.front().rhs.lhs;
+			int sj = cp.front().rhs.rhs;
+			int sc = cp.front().lhs;
+			cp.pop();
+
+			// 모든 더러운 칸을 청소한 상태일 떄 종료
+			if (sc == (1 << dirtyPos.size()) - 1)
+			{
+				elp(dis[si][sj][sc] - 1);
+				isClean = true;
+				break;
+			}
+
 			loop(i, home, 4)
 			{
-				int ci = s.lhs + cd[i].lhs;
-				int cj = s.rhs + cd[i].rhs;
+				int ci = si + cd[i].lhs;
+				int cj = sj + cd[i].rhs;
+				int cc = sc;
+
 				if (ci < home || cj < home || ci >= n || cj >= m) continue;
-				if (dis[ci][cj] > home) continue;
 				if (graph[ci][cj] == 'x') continue;
-				cp.push({ ci, cj });
-				dis[ci][cj] = dis[s.lhs][s.rhs] + 1;
-			}
-		}
-		bool isClean = true;
-		loop(i, home, dirtyPos.size())
-		{
-			dMap[{home, i + 1}] = dis[dirtyPos[i].lhs][dirtyPos[i].rhs] - 1;
-			if (dMap[{home, i + 1}] == -1) isClean = false;
-		}
 
-		// 더러운 칸 <-> 더러운 칸 메모
-		loop(i, home, dirtyPos.size())
-		{
-			loop(j, home, n) loop(k, home, m) dis[j][k] = home;
-			cp.push(dirtyPos[i]);
-			dis[dirtyPos[i].lhs][dirtyPos[i].rhs] = 1;
-			while (!cp.empty())
-			{
-				pii s = cp.front(); cp.pop();
-				loop(j, home, 4)
+				// 더러운 칸
+				if (graph[ci][cj] == '*')
 				{
-					int ci = s.lhs + cd[j].lhs;
-					int cj = s.rhs + cd[j].rhs;
-					if (ci < home || cj < home || ci >= n || cj >= m) continue;
-					if (dis[ci][cj] > home) continue;
-					if (graph[ci][cj] == 'x') continue;
-					cp.push({ ci, cj });
-					dis[ci][cj] = dis[s.lhs][s.rhs] + 1;
+					loop(j, home, dirtyPos.size())
+					{
+						if (ci == dirtyPos[j].lhs && cj == dirtyPos[j].rhs)
+						{
+							// 현재 더러운 칸을 청소한 조합
+							cc |= (1 << j);
+
+							if (dis[ci][cj][cc] <= dis[si][sj][sc] + 1) continue;
+
+							cp.push({ cc, {ci, cj} });
+							dis[ci][cj][cc] = dis[si][sj][sc] + 1;
+						}
+					}
+
+					continue;
 				}
+
+				// 빈공간
+				if (dis[ci][cj][sc] <= dis[si][sj][sc] + 1) continue;
+
+				cp.push({ sc, {ci, cj} });
+				dis[ci][cj][sc] = dis[si][sj][sc] + 1;
 			}
-			loop(j, home, dirtyPos.size())
-			{
-				dMap[{i + 1, j + 1}] = dis[dirtyPos[j].lhs][dirtyPos[j].rhs] - 1;
-				if (dMap[{i + 1, j + 1}] == -1) isClean = false;
-			}
 		}
 
-		// 더러운 칸 없을 때
-		if (dirtyPos.empty())
-		{
-			// 초기화
-			loop(i, home, dirtyPos.size()) vis[i] = false;
-			dirtyPos.clear();
-			dMap.clear();
-			moveCnt = cleanCnt = home;
-			ans = MAX;
-
-			elp(home);
-			continue;
-		}
-
-		// 청소 못 하는 더러운 칸이 있을 때
-		if (!isClean)
-		{
-			// 초기화
-			loop(i, home, dirtyPos.size()) vis[i] = false;
-			dirtyPos.clear();
-			dMap.clear();
-			moveCnt = cleanCnt = home;
-			ans = MAX;
-
-			elp(-1);
-			continue;
-		}
-
-		// 재사용
-		DFS(home);
-
-		elp(cond(ans == MAX, -1, ans));
-
-		// 초기화
-		loop(i, home, dirtyPos.size()) vis[i] = false;
-		dirtyPos.clear();
-		dMap.clear();
-		moveCnt = cleanCnt = home;
-		ans = MAX;
+		if (isClean) continue;
+		elp(-1);
 	}
 
 	return home;
