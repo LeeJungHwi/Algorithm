cffd893d12de6c5c8d0d59b7fa70f8760654a56c
JungHwi <wnd4114294225@gmail.com>
2025-03-02 19:19:11 +0900

    [Gold I] Title: Gaaaaaaaaaarden, Time: 336 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Gold/18809. Gaaaaaaaaaarden/Gaaaaaaaaaarden.cc
+++ 백준/Gold/18809. Gaaaaaaaaaarden/Gaaaaaaaaaarden.cc
@@ -1,215 +1,184 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <string>
-#include <map>
-#include <fstream>
-#include <tuple>
+#include <bits/stdc++.h>
 using namespace std;
 
-int n, m, g, r; // N, M, G, R 3, 3, 2, 1
-vector<vector<int>> graph(50, vector<int>(50)); // 그래프
-vector<pair<int, int>> soilPos; // 황토색 땅 위치 (0, 0) (2, 0) (2, 2)
-vector<pair<pair<int, int>, int> > selected; // (황토색 땅 위치, 뿌려잰 배양액의 색)
-int maxFlowerCnt; // 피울수있는 꽃의 최대 개수
+#define home 0
 
-vector<pair<int, int> > checkDir = { {-1,0}, {1, 0}, {0, -1}, {0, 1} }; // 상하좌우
-queue<pair<int, int> > checkPos; // 체크 할 위치
+#ifdef ONLINE_JUDGE
+#define init ios_base::sync_with_stdio(home); cin.tie(home)
+#else
+#define init ios_base::sync_with_stdio(home); cin.tie(home); ifstream cin("input.txt")
+#endif
 
-// BFS
-// 각 경우의 수에 대해 배양액이 퍼져나가면서 피울수있는 꽃의 최대개수 갱신하기
-void BFS()
+#define ll long long
+#define ld long double
+
+#define pii pair<int, int>
+#define piii pair<int, pii>
+#define pll pair<ll, ll>
+#define plll pair<ll, pll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define tvec(t, v) vector<t> v
+#define vec(t, v, r) vector<t> v((r))
+#define ivec(t, v, r, i) vector<t> v((r), i)
+#define gmat(t, v, r) vector<vector<t> > v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+#define imat(t, v, r, c, i) vector<vector<t> > v((r), vector<t>((c), i))
+#define smat(t, v, r, c, s) vector<vector<vector<t> > > v((r), vector<vector<t>>((c), vector<t>((s))))
+#define ismat(t, v, r, c, s, i) vector<vector<vector<t> > > v((r), vector<vector<t>>((c), vector<t>((s), i)))
+#define ssmat(t, v, r, c, s1, s2) vector<vector<vector<vector<t> > > > v((r), vector<vector<vector<t>>>((c), vector<vector<t>>((s1), vector<t>((s2)))))
+#define issmat(t, v, r, c, s1, s2, i) vector<vector<vector<vector<t> > > > v((r), vector<vector<vector<t>>>((c), vector<vector<t>>((s1), vector<t>((s2), i))))
+#define sssmat(t, v, r, c, s1, s2, s3) vector<vector<vector<vector<vector<t> > > > > v((r), vector<vector<vector<vector<t>>>>((c), vector<vector<vector<t>>>((s1), vector<vector<t>>((s2), vector<t>((s3))))))
+#define isssmat(t, v, r, c, s1, s2, s3, i) vector<vector<vector<vector<vector<t> > > > > v((r), vector<vector<vector<vector<t>>>>((c), vector<vector<vector<t>>>((s1), vector<vector<t>>((s2), vector<t>((s3), i)))))
+
+#define dir vector<pii> cd = { {-1, home}, {1, home}, { home, -1 }, { home, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+#define kdir vector<pii> kcd = { {-1, -2}, {-2, -1}, { -2, 1 }, { -1, 2 }, { 1, -2 }, { 2, -1 }, { 1, 2 }, { 2, 1 } }
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
+
+int n, m, g, r;
+queue<pii> cp; dir;
+vector<pii> sPos;
+vector<int> order, gorder;
+int ans = home;
+
+// g개 비중복조합
+void GDFS(int L, int s, int bitmask, vector<vector<int>> &graph, vector<vector<int>> &dis, vector<vector<int>>&temp)
 {
-	int flowerCnt = 0; // 각 경우의 수에 대한 꽃이 피는 수
-	vector<vector<int>> dis(n, vector<int>(m)); // 거리체크
-	vector<vector<int>> vis(n, vector<int>(m)); // 방문체크
-	vector<pair<int, int> > flowerPos; // 꽃이 핀 위치 저장
-
-	// 모든 배양액부터 BFS
-	for (int i = 0; i < selected.size(); i++)
+	if (L == g)
 	{
-		checkPos.push(selected[i].first);
-		dis[selected[i].first.first][selected[i].first.second] = 1;
-		vis[selected[i].first.first][selected[i].first.second] = selected[i].second;
-	}
+		// g 1
+		loop(i, home, gorder.size()) temp[sPos[order[gorder[i]]].lhs][sPos[order[gorder[i]]].rhs] = 1;
 
-	// 큐가 빌때까지
-	while (!checkPos.empty())
-	{
-		// 기준위치 꺼냄
-		pair<int, int> standardPos = checkPos.front();
-		checkPos.pop();
-
-		// 꽃이 핀 위치는 퍼질수 없으므로 continue
-		bool cantSpread = false;
-
-		for (int i = 0; i < flowerPos.size(); i++)
+		// r 2
+		loop(i, home, order.size())
 		{
-			if (standardPos.first == flowerPos[i].first && standardPos.second == flowerPos[i].second)
-			{
-				cantSpread = true;
+			if (temp[sPos[order[i]].lhs][sPos[order[i]].rhs] == home) temp[sPos[order[i]].lhs][sPos[order[i]].rhs] = 2;
 
-				break;
-			}
+			// 배양액 떨어진 곳 큐에 저장
+			cp.push(sPos[order[i]]);
+			dis[sPos[order[i]].lhs][sPos[order[i]].rhs] = 1;
 		}
 
-		if (cantSpread)
+		// 멀티 소스 BFS
+		int flowerCnt = home;
+		while (!cp.empty())
 		{
-			continue;
-		}
+			int si = cp.front().lhs;
+			int sj = cp.front().rhs;
+			cp.pop();
 
-		// 상하좌우
-		for (int i = 0; i < 4; i++)
-		{
-			// 체크 할 위치
-			int checkI = standardPos.first + checkDir[i].first;
-			int checkJ = standardPos.second + checkDir[i].second;
+			// 꽃이 핀 위치는 퍼질 수 없음
+			if (temp[si][sj] == 3) continue;
 
-			// 경계체크
-			if (checkI < 0 || checkJ < 0 || checkI >= n || checkJ >= m)
+			loop(i, home, 4)
 			{
-				continue;
-			}
+				int ci = si + cd[i].lhs;
+				int cj = sj + cd[i].rhs;
 
-			// 호수체크
-			if (graph[checkI][checkJ] == 0)
-			{
-				continue;
-			}
+				if (ci < home || cj < home || ci >= n || cj >= m) continue;
+				if (graph[ci][cj] == home) continue;
 
-			// 아직 어떤 배양액도 없는 땅이면 배양액 저장, 거리 증가, 큐에 추가
-			if (vis[checkI][checkJ] == 0)
-			{
-				vis[checkI][checkJ] = vis[standardPos.first][standardPos.second];
-				dis[checkI][checkJ] = dis[standardPos.first][standardPos.second] + 1;
-				checkPos.push({ checkI, checkJ });
-			} 
-			else // 이미 배양액이 있는 땅이면
-			{
-				// 서로 다른 배양색이면서
-				if (vis[checkI][checkJ] != vis[standardPos.first][standardPos.second])
+				// 아직 배양액이 퍼지지 않은 곳
+				if (dis[ci][cj] == home)
 				{
-					// 동일한 시간에 도달하고 아직 꽃을 피운상태가 아니면 꽃을 피움
-					if (dis[checkI][checkJ] == dis[standardPos.first][standardPos.second] + 1 && vis[checkI][checkJ] != 3)
+					cp.push({ ci, cj });
+					dis[ci][cj] = dis[si][sj] + 1;
+					temp[ci][cj] = temp[si][sj];
+				}
+				// 배양액이 퍼진 곳 => 동시에 도착한 서로 다른 배양액이 꽃이 피지 않았으면 꽃이 핌
+				else if(dis[ci][cj] == dis[si][sj] + 1)
+				{
+					if (temp[ci][cj] != temp[si][sj] && temp[ci][cj] != 3)
 					{
-						flowerCnt++; // 핀 꽃 수 카운팅
-						vis[checkI][checkJ] = 3; // 꽃을 피운 상태
-						flowerPos.push_back({ checkI, checkJ }); // 꽃이 핀 위치 저장
+						flowerCnt++;
+						temp[ci][cj] = 3;
 					}
 				}
 			}
 		}
+
+		// 꽃이 피는 최대 개수 업데이트
+		ans = max(ans, flowerCnt);
+
+		// 거리 및 temp 그래프 초기화
+		loop(i, home, n) loop(j, home, m)
+		{
+			dis[i][j] = home;
+			temp[i][j] = home;
+		}
 	}
+	else
+	{
+		loop(i, s, order.size())
+		{
+			if ((bitmask & (1 << i)) > home) continue;
 
-	// 디버깅용
-	//for (auto a : vis)
-	//{
-	//	for (auto b : a)
-	//	{
-	//		cout << b << ' ';
-	//	}
-	//	cout << '\n';
-	//}
-	//cout << '\n';
-
-	maxFlowerCnt = max(maxFlowerCnt, flowerCnt);
+			gorder.push_back(i);
+			GDFS(L + 1, i, bitmask | (1 << i), graph, dis, temp);
+			gorder.pop_back();
+		}
+	}
 }
 
-// DFS
-// 황토색 땅에 배양액을 놓는 경우의 수 구하기
-void DFS(int L, int cntG, int cntR)
+// g + r개 비중복조합
+void DFS(int L, int s, int bitmask, vector<vector<int>> &graph, vector<vector<int>> &dis, vector<vector<int>> &temp)
 {
-	if (L == soilPos.size() || cntG == g && cntR == r) // 종료조건 : 황토색 땅 위치 수를 넘겼거나 모든 배양액을 뿌린경우
+	if (L == g + r)
 	{
-		// 황토색 땅 위치수를 넘긴경우
-		if (L == soilPos.size())
+		GDFS(home, home, home, graph, dis, temp);
+	}
+	else
+	{
+		loop(i, s, sPos.size())
 		{
-			// 모든 배양액을 뿌린경우가 아니면 리턴
-			if (cntG != g || cntR != r)
-			{
-				return;
-			}
+			if ((bitmask & (1 << i)) > home) continue;
+
+			order.push_back(i);
+			DFS(L + 1, i, bitmask | (1 << i), graph, dis, temp);
+			order.pop_back();
 		}
-
-		// 디버깅용
-		//for (int i = 0; i < selected.size(); i++)
-		//{
-		//	cout << "(" << selected[i].first.first << "," << selected[i].first.second << ")" << selected[i].second;
-		//}
-		//cout << '\n';
-
-		// 모든 배양액을 뿌렸으면
-		// BFS 돌려서 꽃을 피울수있는 최대개수 갱신
-		BFS();
-
-		return;
 	}
-
-	// 초록색 배양액 처리
-	if (cntG < g)
-	{
-		selected.push_back({ soilPos[L], 1 });
-		DFS(L + 1, cntG + 1, cntR);
-		selected.pop_back();
-	}
-
-	// 빨간색 배양액 처리
-	if (cntR < r)
-	{
-		selected.push_back({ soilPos[L], 2 });
-		DFS(L + 1, cntG, cntR + 1);
-		selected.pop_back();
-	}
-
-	// 해당 황토색 땅 건너뜀
-	DFS(L + 1, cntG, cntR);
 }
 
 // Gaaaaaaaaaarden
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	//ifstream cin;
-	//cin.open("input.txt");
+	init;
 
-	// 하얀색 1 : 배양액 뿌릴수없음
-	// 황토색 2 : 배양액 뿌릴수있음
-	// 하늘색 0 : 호수
-
-	// 초록색 배양액과 빨간색 배양액이 동일한 시간에 땅에 도달시 두 배양액이 합쳐져 꽃이 핀다
-	// 꽃이 핀 땅에서는 배양액이 사라지고 더이상 인접한 땅으로 배양액이 퍼지지않는다
-	// 주어진 배양액은 서로 다른곳에 모두 뿌려져야한다
-
-	// 정원과 두 배양액의 개수가 주어지면 피울수있는 꽃의 최대 개수 구하기
-
-	// 1. DFS로 g와 r을 놓을수있는 경우의 수 구하기(황토색 땅의 서로다른곳에 뿌림)
-	// 2. BFS로 각 경우의 수에 대해 꽃을 피울수있는 최대 개수 갱신
-
+	// 배양액을 뿌릴 수 있는 땅에 초록색 배양액과 빨간색 배양액을 뿌리는 비중복조합
+	// 빨간색 배양액과 초록색 배양액이 동시에 도달하는 위치에 꽃이피고 퍼트리지 않음
+	
+	// 1.배양액을 뿌릴 수 있는 땅에서 g + r개의 땅을 뽑음
+	// 2.뽑은 땅 g + r개에서 g개의 땅을 뽑아서 temp 그래프에 1 나머지 뽑히지 않은 땅은 temp 그래프에 2로
+	// 3.모든 배양액 부터 멀티 소스 BFS 돌려서 동시에 도달하면서 서로 다른 배양액이면 꽃이피고 분기 X
 	cin >> n >> m >> g >> r;
-
-	//	2 1 0
-	//	1 0 1
-	//	2 1 2
-	// 황토색 땅 위치 저장해둠
-	for (int i = 0; i < n; i++)
+	mat(int, graph, n, m);
+	mat(int, dis, n, m);
+	mat(int, temp, n, m);
+	loop(i, home, n) loop(j, home, m)
 	{
-		for (int j = 0; j < m; j++)
-		{
-			cin >> graph[i][j];
-
-			if (graph[i][j] == 2)
-			{
-				soilPos.push_back({ i, j });
-			}
-		}
+		cin >> graph[i][j];
+		if (graph[i][j] == 2) sPos.push_back({ i, j });
 	}
 
-	// DFS
-	DFS(0, 0, 0);
+	DFS(home, home, home, graph, dis, temp);
+	elp(ans);
 
-	cout << maxFlowerCnt << '\n';
-
-	return 0;
+	return home;
 }
