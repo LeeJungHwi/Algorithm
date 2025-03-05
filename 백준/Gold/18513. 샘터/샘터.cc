13d9d231ce3ec6baed10da4b7defb9d973bb1fe6
JungHwi <wnd4114294225@gmail.com>
2025-03-05 14:24:39 +0900

    [Gold IV] Title: 샘터, Time: 188 ms, Memory: 15888 KB -BaekjoonHub

---
+++ 백준/Gold/18513. 샘터/샘터.cc
@@ -0,0 +1,109 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define home 0
+
+#ifdef ONLINE_JUDGE
+#define init ios_base::sync_with_stdio(home); cin.tie(home)
+#else
+#define init ios_base::sync_with_stdio(home); cin.tie(home); ifstream cin("input.txt")
+#endif
+
+#define ll long long
+#define ld long double
+
+#define pii pair<int, int>
+#define piii pair<int, pii>
+#define pll pair<ll, ll>
+#define plll pair<ll, pll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define tvec(t, v) vector<t> v
+#define vec(t, v, r) vector<t> v((r))
+#define ivec(t, v, r, i) vector<t> v((r), i)
+#define gmat(t, v, r) vector<vector<t> > v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+#define imat(t, v, r, c, i) vector<vector<t> > v((r), vector<t>((c), i))
+#define smat(t, v, r, c, s) vector<vector<vector<t> > > v((r), vector<vector<t>>((c), vector<t>((s))))
+#define ismat(t, v, r, c, s, i) vector<vector<vector<t> > > v((r), vector<vector<t>>((c), vector<t>((s), i)))
+#define ssmat(t, v, r, c, s1, s2) vector<vector<vector<vector<t> > > > v((r), vector<vector<vector<t>>>((c), vector<vector<t>>((s1), vector<t>((s2)))))
+#define issmat(t, v, r, c, s1, s2, i) vector<vector<vector<vector<t> > > > v((r), vector<vector<vector<t>>>((c), vector<vector<t>>((s1), vector<t>((s2), i))))
+#define sssmat(t, v, r, c, s1, s2, s3) vector<vector<vector<vector<vector<t> > > > > v((r), vector<vector<vector<vector<t>>>>((c), vector<vector<vector<t>>>((s1), vector<vector<t>>((s2), vector<t>((s3))))))
+#define isssmat(t, v, r, c, s1, s2, s3, i) vector<vector<vector<vector<vector<t> > > > > v((r), vector<vector<vector<vector<t>>>>((c), vector<vector<vector<t>>>((s1), vector<vector<t>>((s2), vector<t>((s3), i)))))
+
+#define dir vector<pii> cd = { {-1, home}, {1, home}, { home, -1 }, { home, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+#define kdir vector<pii> kcd = { {-1, -2}, {-2, -1}, { -2, 1 }, { -1, 2 }, { 1, -2 }, { 2, -1 }, { 1, 2 }, { 2, 1 } }
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
+
+// 샘터
+int main()
+{
+	init;
+
+	// 집의 불행도 => 샘터와의 거리
+	// 불행도 합의 최솟값 => 모든 샘터 부터 멀티 소스 BFS 돌려서 가까운 집 k개를 설치하고 불행도 합 구하기
+
+	ll n, k; cin >> n >> k;
+	map<ll, ll> dis;
+	queue<ll> cp;
+	vec(ll, d, 2);
+
+	ll sPos;
+	loop(i, home, n)
+	{
+		cin >> sPos;
+		cp.push(sPos);
+		dis[sPos] = 1;
+	}
+
+	ll ans = home; // 불행도 합 최솟값
+	while (!cp.empty())
+	{
+		ll s = cp.front();
+		cp.pop();
+
+		d[home] = s - 1;
+		d[1] = s + 1;
+
+		loop(i, home, 2)
+		{
+			ll c = d[i];
+
+			//if (c < -100000000 || c > 100000000) continue;
+			if (dis[c] > home) continue;
+
+			cp.push(c);
+			dis[c] = dis[s] + 1;
+			
+			// 샘터와 가깝게 k개의 집을 설치하면 종료
+			if (k > home)
+			{
+				ans += dis[c] - 1;
+				k--;
+			}
+			else
+			{
+				elp(ans);
+				return home;
+			}
+		}
+	}
+
+	return home;
+}
