93db1bcef4956389037c6ea0374055e9d56a3450
JungHwi <wnd4114294225@gmail.com>
2025-03-06 22:57:26 +0900

    [Silver III] Title: 카드 문자열, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/13417. 카드 문자열/카드 문자열.cc
+++ 백준/Silver/13417. 카드 문자열/카드 문자열.cc
@@ -2,34 +2,75 @@
 using namespace std;
 
 #define home 0
-#define loop(v, s, e) for(int v = s; v < e; v+=2)
-#define elprint(a) cout << a << '\n'
+
+#ifdef ONLINE_JUDGE
+#define init ios_base::sync_with_stdio(home); cin.tie(home)
+#else
+#define init ios_base::sync_with_stdio(home); cin.tie(home); ifstream cin("input.txt")
+#endif
+
+#define ll long long
+#define ld long double
+
+#define pii pair<int, int>
+#define piii pair<int, pii>
+#define pll pair<ll, ll>
+#define plll pair<ll, pll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define tvec(t, v) vector<t> v
+#define vec(t, v, r) vector<t> v((r))
+#define ivec(t, v, r, i) vector<t> v((r), i)
+#define gmat(t, v, r) vector<vector<t> > v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+#define imat(t, v, r, c, i) vector<vector<t> > v((r), vector<t>((c), i))
+#define smat(t, v, r, c, s) vector<vector<vector<t> > > v((r), vector<vector<t>>((c), vector<t>((s))))
+#define ismat(t, v, r, c, s, i) vector<vector<vector<t> > > v((r), vector<vector<t>>((c), vector<t>((s), i)))
+#define ssmat(t, v, r, c, s1, s2) vector<vector<vector<vector<t> > > > v((r), vector<vector<vector<t>>>((c), vector<vector<t>>((s1), vector<t>((s2)))))
+#define issmat(t, v, r, c, s1, s2, i) vector<vector<vector<vector<t> > > > v((r), vector<vector<vector<t>>>((c), vector<vector<t>>((s1), vector<t>((s2), i))))
+#define sssmat(t, v, r, c, s1, s2, s3) vector<vector<vector<vector<vector<t> > > > > v((r), vector<vector<vector<vector<t>>>>((c), vector<vector<vector<t>>>((s1), vector<vector<t>>((s2), vector<t>((s3))))))
+#define isssmat(t, v, r, c, s1, s2, s3, i) vector<vector<vector<vector<vector<t> > > > > v((r), vector<vector<vector<vector<t>>>>((c), vector<vector<vector<t>>>((s1), vector<vector<t>>((s2), vector<t>((s3), i)))))
+
+#define dir vector<pii> cd = { {-1, home}, {1, home}, { home, -1 }, { home, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+#define kdir vector<pii> kcd = { {-1, -2}, {-2, -1}, { -2, 1 }, { -1, 2 }, { 1, -2 }, { 2, -1 }, { 1, 2 }, { 2, 1 } }
+#define lhs first
+#define rhs second
+
 #define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
 
 // 카드 문자열
 int main()
 {
-	ios_base::sync_with_stdio(home);
-	cin.tie(home);
-	//ifstream cin;
-	//cin.open("input.txt");
+	init;
 
-	int tc;
-	cin >> tc;
-
+	// 현재 문자열의 첫 요소보다 작으면 앞에 추가 아니면 뒤에 추가
+	int tc; cin >> tc;
+	
 	while (tc--)
 	{
-		int n;
-		cin >> n;
-		cin.ignore();
-		string inputString;
-		getline(cin, inputString);
-		string outputString = "";
-		outputString += inputString[0];
+		string ans = "";
 
-		loop(i, 2, inputString.size()) outputString = cond(inputString[i] <= outputString.front(), inputString[i] + outputString, outputString + inputString[i]);
-
-		elprint(outputString);
+		int n; cin >> n;
+		char ic;
+		loop(i, home, n)
+		{
+			cin >> ic;
+			ans = cond(ans.empty() || ans.front() >= ic, ic + ans, ans + ic);
+		}
+		elp(ans);
 	}
 
 	return home;
