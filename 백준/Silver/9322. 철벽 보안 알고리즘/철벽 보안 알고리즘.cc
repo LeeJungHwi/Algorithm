f9d7840bc0a8c035c78a8c5692d1ddbab0bced2b
JungHwi <wnd4114294225@gmail.com>
2025-03-08 10:59:55 +0900

    [Silver IV] Title: 철벽 보안 알고리즘, Time: 44 ms, Memory: 2308 KB -BaekjoonHub

--- 백준/Silver/9322. 철벽 보안 알고리즘/철벽 보안 알고리즘.cc
+++ 백준/Silver/9322. 철벽 보안 알고리즘/철벽 보안 알고리즘.cc
@@ -2,91 +2,98 @@
 using namespace std;
 
 #define home 0
+
+#ifdef ONLINE_JUDGE
+#define init ios_base::sync_with_stdio(home); cin.tie(home)
+#else
+#define init ios_base::sync_with_stdio(home); cin.tie(home); ifstream cin("input.txt")
+#endif
+
 #define ll long long
+#define ld long double
+
 #define pii pair<int, int>
-#define loop(v, s, e) for(int v = s; v < e; v++)
-#define rloop(v, s, e) for(int v = s; v > e; v--)
-#define mloop(t, v, s, e, c, i) for(t v = s; v c e; i)
-#define elprint(a) cout << a << '\n'
-#define scprint(a) cout << a << ' '
+#define piii pair<int, pii>
+#define pll pair<ll, ll>
+#define plll pair<ll, pll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define tvec(t, v) vector<t> v
+#define vec(t, v, r) vector<t> v((r))
+#define ivec(t, v, r, i) vector<t> v((r), i)
+#define gmat(t, v, r) vector<vector<t> > v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+#define imat(t, v, r, c, i) vector<vector<t> > v((r), vector<t>((c), i))
+#define smat(t, v, r, c, s) vector<vector<vector<t> > > v((r), vector<vector<t>>((c), vector<t>((s))))
+#define ismat(t, v, r, c, s, i) vector<vector<vector<t> > > v((r), vector<vector<t>>((c), vector<t>((s), i)))
+#define ssmat(t, v, r, c, s1, s2) vector<vector<vector<vector<t> > > > v((r), vector<vector<vector<t>>>((c), vector<vector<t>>((s1), vector<t>((s2)))))
+#define issmat(t, v, r, c, s1, s2, i) vector<vector<vector<vector<t> > > > v((r), vector<vector<vector<t>>>((c), vector<vector<t>>((s1), vector<t>((s2), i))))
+#define sssmat(t, v, r, c, s1, s2, s3) vector<vector<vector<vector<vector<t> > > > > v((r), vector<vector<vector<vector<t>>>>((c), vector<vector<vector<t>>>((s1), vector<vector<t>>((s2), vector<t>((s3))))))
+#define isssmat(t, v, r, c, s1, s2, s3, i) vector<vector<vector<vector<vector<t> > > > > v((r), vector<vector<vector<vector<t>>>>((c), vector<vector<vector<t>>>((s1), vector<vector<t>>((s2), vector<t>((s3), i)))))
+
+#define dir vector<pii> cd = { {-1, home}, {1, home}, { home, -1 }, { home, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+#define kdir vector<pii> kcd = { {-1, -2}, {-2, -1}, { -2, 1 }, { -1, 2 }, { 1, -2 }, { 2, -1 }, { 1, 2 }, { 2, 1 } }
+#define lhs first
+#define rhs second
+
 #define cond(c, t, f) ((c) ? (t) : (f))
-#define mat(t, v, r, c) vector<vector<t> > v(r, vector<t>(c))
-#define X first
-#define Y second
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
 
 // 철벽 보안 알고리즘
 int main()
 {
-	ios_base::sync_with_stdio(home);
-	cin.tie(home);
-	//ifstream cin;
-	//cin.open("input.txt");
+	init;
 
-	int tc;
-	cin >> tc;
+	// 2공개키 => 1공개키를 재배치
+	// 암호문 => 평문을 재배치
+	int tc; cin >> tc;
 
 	while (tc--)
 	{
-		int n;
-		cin >> n;
+		int n; cin >> n;
+		map<string, pii> wordMap; // (단어, (1공개키 위치, 2공개키 위치)) 맵핑
+		map<int, int> m2; // (2공개키 위치, 1공개키 위치) 맵핑
+		string is;
 
-		cin.ignore();
-
-		map<int, string> fMap;
-		map<string, int> sMap;
-		map<int, int> tMap;
-		map<int, string> oMap;
-
-		string inputString;
-		getline(cin, inputString);
-		inputString += ' ';
-		string temp = "";
-		int cur = 0;
-		loop(i, 0, inputString.size())
+		// 단어, 1공개키 맵핑
+		loop(i, home, n)
 		{
-			if (inputString[i] == ' ')
-			{
-				fMap[++cur] = temp;
-				sMap[temp] = cur;
-				temp.clear();
-				continue;
-			}
-
-			temp += inputString[i];
+			cin >> is;
+			wordMap[is].lhs = i + 1;
 		}
 
-		getline(cin, inputString);
-		inputString += ' ';
-		cur = 0;
-		loop(i, 0, inputString.size())
+		// 단어, 2공개키 맵핑
+		loop(i, home, n)
 		{
-			if (inputString[i] == ' ')
-			{
-				tMap[++cur] = sMap[temp];
-				temp.clear();
-				continue;
-			}
-
-			temp += inputString[i];
+			cin >> is;
+			wordMap[is].rhs = i + 1;
 		}
 
-		getline(cin, inputString);
-		inputString += ' ';
-		cur = 0;
-		loop(i, 0, inputString.size())
-		{
-			if (inputString[i] == ' ')
-			{
-				oMap[tMap[++cur]] = temp;
-				temp.clear();
-				continue;
-			}
+		// 2공개키, 1공개키 맵핑
+		mloop(it, wordMap) m2[it->rhs.rhs] = it->rhs.lhs;
 
-			temp += inputString[i];
+		// 암호문으로 평문 해독
+		vec(string, ans, n);
+		loop(i, home, n)
+		{
+			cin >> is;
+			ans[m2[i + 1] - 1] = is;
 		}
 
-		mloop(auto, it, oMap.begin(), oMap.end(), != , it++) scprint(it->second);
-		elprint("");
+		loop(i, home, n) scp(ans[i]);
+		cout << '\n';
 	}
 
 	return home;
