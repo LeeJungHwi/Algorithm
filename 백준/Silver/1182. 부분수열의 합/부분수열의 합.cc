31a6ef10c5718afbdde0341715f3c840fbf3e8c8
JungHwi <wnd4114294225@gmail.com>
2025-03-20 20:54:12 +0900

    [Silver II] Title: 부분수열의 합, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -1,85 +1,87 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <string>
-#include <map>
-#include <fstream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int n, s; // N, M 5, 0
-vector<int> nums; // 정수 저장 -7 -3 -2 5 8
-vector<bool> vis(20); // 방문체크
-int cnt; // 부분수열합이 s가되는 개수
+#define home 0
 
-// DFS
-void DFS(int L)
+#ifdef ONLINE_JUDGE
+#define init ios_base::sync_with_stdio(home); cin.tie(home)
+#else
+#define init ios_base::sync_with_stdio(home); cin.tie(home); ifstream cin("input.txt")
+#endif
+
+#define ll long long
+#define ld long double
+
+#define pii pair<int, int>
+#define piii pair<int, pii>
+#define pll pair<ll, ll>
+#define plll pair<ll, pll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define tvec(t, v) vector<t> v
+#define vec(t, v, r) vector<t> v((r))
+#define ivec(t, v, r, i) vector<t> v((r), i)
+#define gmat(t, v, r) vector<vector<t> > v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+#define imat(t, v, r, c, i) vector<vector<t> > v((r), vector<t>((c), i))
+#define smat(t, v, r, c, s) vector<vector<vector<t> > > v((r), vector<vector<t>>((c), vector<t>((s))))
+#define ismat(t, v, r, c, s, i) vector<vector<vector<t> > > v((r), vector<vector<t>>((c), vector<t>((s), i)))
+#define ssmat(t, v, r, c, s1, s2) vector<vector<vector<vector<t> > > > v((r), vector<vector<vector<t>>>((c), vector<vector<t>>((s1), vector<t>((s2)))))
+#define issmat(t, v, r, c, s1, s2, i) vector<vector<vector<vector<t> > > > v((r), vector<vector<vector<t>>>((c), vector<vector<t>>((s1), vector<t>((s2), i))))
+#define sssmat(t, v, r, c, s1, s2, s3) vector<vector<vector<vector<vector<t> > > > > v((r), vector<vector<vector<vector<t>>>>((c), vector<vector<vector<t>>>((s1), vector<vector<t>>((s2), vector<t>((s3))))))
+#define isssmat(t, v, r, c, s1, s2, s3, i) vector<vector<vector<vector<vector<t> > > > > v((r), vector<vector<vector<vector<t>>>>((c), vector<vector<vector<t>>>((s1), vector<vector<t>>((s2), vector<t>((s3), i)))))
+
+#define dir vector<pii> cd = { {-1, home}, {1, home}, { home, -1 }, { home, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+#define kdir vector<pii> kcd = { {-1, -2}, {-2, -1}, { -2, 1 }, { -1, 2 }, { 1, -2 }, { 2, -1 }, { 1, 2 }, { 2, 1 } }
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
+
+int n, s, sum, ans;
+
+void DFS(int L, vector<int> &v)
 {
-	if (L == n) // 종료조건 : 선택할수있는 정수 개수를 넘긴경우
-	{
-		// sum이 s가 되면 개수 카운팅
+	// 범위를 넘어가면 X
+	if (L == n) return;
 
-		int sum = 0;
+	// 선택
+	// 합이 s면 카운팅
+	sum += v[L];
+	if (sum == s) ans++;
+	DFS(L + 1, v);
 
-		for (int i = 0; i < n; i++)
-		{
-			if (vis[i])
-			{
-				sum += nums[i];
-			}
-		}
-
-		if (sum == s)
-		{
-			cnt++;
-		}
-
-		return;
-	}
-	else
-	{
-		vis[L] = true; // 선택
-		DFS(L + 1);
-		vis[L] = false; // 미선택
-		DFS(L + 1);
-	}
+	// 미선택
+	sum -= v[L];
+	DFS(L + 1, v);
 }
 
-// 부분수열 합
+// 부분수열의 합
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	//ifstream cin;
-	//cin.open("input.txt");
+	init;
 
+	// 현재 수를 선택 하거나 선택 하지 않거나로 분기
+	// 범위를 넘어가면 가지치기
 	cin >> n >> s;
-
-	// -7 -3 -2 5 8
-
-	int num; // 숫자
-
-	for (int i = 0; i < n; i++)
-	{
-		cin >> num;
-
-		nums.push_back(num);
-	}
-
-	// DFS
-	DFS(0);
+	vec(int, v, n);
+	loop(i, home, n) cin >> v[i];
 	
-	// s가 0이면 cnt - 1 출력
-	// 아니면 cnt 출력
+	DFS(home, v);
+	elp(ans);
 
-	if (s == 0)
-	{
-		cout << cnt - 1 << '\n';
-
-		return 0;
-	}
-
-	cout << cnt << '\n';
-
-	return 0;
+	return home;
 }
