de750e02ca4fad4130c383c302df6d19965cf8b3
JungHwi <wnd4114294225@gmail.com>
2025-03-28 13:41:53 +0900

    [Silver II] Title: 영재의 시험, Time: 36 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/19949. 영재의 시험/영재의 시험.cc
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define home 0
+
+#ifdef ONLINE_JUDGE
+#define init ios_base::sync_with_stdio(home); cin.tie(home)
+#else
+#define init ios_base::sync_with_stdio(home); cin.tie(home); ifstream cin("input.txt")
+#endif
+
+#define ll long long
+#define ld long double
+
+#define pii pair<int, int>
+#define piii pair<int, pii>
+#define pll pair<ll, ll>
+#define plll pair<ll, pll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define tvec(t, v) vector<t> v
+#define vec(t, v, r) vector<t> v((r))
+#define ivec(t, v, r, i) vector<t> v((r), i)
+#define gmat(t, v, r) vector<vector<t> > v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+#define imat(t, v, r, c, i) vector<vector<t> > v((r), vector<t>((c), i))
+#define smat(t, v, r, c, s) vector<vector<vector<t> > > v((r), vector<vector<t>>((c), vector<t>((s))))
+#define ismat(t, v, r, c, s, i) vector<vector<vector<t> > > v((r), vector<vector<t>>((c), vector<t>((s), i)))
+#define ssmat(t, v, r, c, s1, s2) vector<vector<vector<vector<t> > > > v((r), vector<vector<vector<t>>>((c), vector<vector<t>>((s1), vector<t>((s2)))))
+#define issmat(t, v, r, c, s1, s2, i) vector<vector<vector<vector<t> > > > v((r), vector<vector<vector<t>>>((c), vector<vector<t>>((s1), vector<t>((s2), i))))
+#define sssmat(t, v, r, c, s1, s2, s3) vector<vector<vector<vector<vector<t> > > > > v((r), vector<vector<vector<vector<t>>>>((c), vector<vector<vector<t>>>((s1), vector<vector<t>>((s2), vector<t>((s3))))))
+#define isssmat(t, v, r, c, s1, s2, s3, i) vector<vector<vector<vector<vector<t> > > > > v((r), vector<vector<vector<vector<t>>>>((c), vector<vector<vector<t>>>((s1), vector<vector<t>>((s2), vector<t>((s3), i)))))
+
+#define dir vector<pii> cd = { {-1, home}, {1, home}, { home, -1 }, { home, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+#define kdir vector<pii> kcd = { {-1, -2}, {-2, -1}, { -2, 1 }, { -1, 2 }, { 1, -2 }, { 2, -1 }, { 1, 2 }, { 2, 1 } }
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
+
+int ans, sum, prev1, prev2;
+
+void DFS(int L, vector<int> &v)
+{
+	if (L == 10)
+	{
+		if (sum >= 5) ans++;
+	}
+	else
+	{
+		loop(i, 1, 6)
+		{
+			// 3개 연속 같은 숫자 X
+			if (i == prev1 && i == prev2) continue;
+
+			// 정답인 경우 맞은 갯수 카운팅
+			if (i == v[L]) sum++;
+
+			// 나중에 돌려줘야하니까 백업
+			int temp = prev2;
+
+			// 이전 숫자 업데이트
+			prev2 = prev1;
+			prev1 = i;
+			
+			DFS(L + 1, v);
+
+			// 백트래킹
+			if (i == v[L]) sum--;
+			prev1 = prev2;
+			prev2 = temp;
+		}
+	}
+}
+
+// 영재의 시험
+int main()
+{
+	init;
+
+	// 중복순열인데 연속으로 3개 이상 같은 수로 찍으면 안되니까 이전 숫자를 추적해야함
+	vec(int, v, 10);
+	loop(i, home, 10) cin >> v[i];
+
+	DFS(home, v);
+	elp(ans);
+
+	return home;
+}
