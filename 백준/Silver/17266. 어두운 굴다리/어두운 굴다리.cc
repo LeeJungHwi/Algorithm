0d5e4bcdd8a83b6367f7cb5d52218d9d23e50830
JungHwi <wnd4114294225@gmail.com>
2025-03-31 22:02:19 +0900

    [Silver IV] Title: 어두운 굴다리, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.cc
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define home 0
+
+#ifdef ONLINE_JUDGE
+#define init ios_base::sync_with_stdio(home); cin.tie(home)
+#else
+#define init ios_base::sync_with_stdio(home); cin.tie(home); ifstream cin("input.txt")
+#endif
+
+#define ll long long
+#define ld long double
+
+#define pii pair<int, int>
+#define piii pair<int, pii>
+#define pll pair<ll, ll>
+#define plll pair<ll, pll>
+
+#define loop(v, s, e) for(int v = (s); v < (e); v++)
+#define rloop(v, s, e) for(int v = (s); v > (e); v--)
+#define mloop(v, a) for(auto v = (a).begin(); v != (a).end(); v++)
+#define mrloop(v, a) for(auto v = (a).rbegin(); v != (a).rend(); v++)
+
+#define p(a) cout << (a)
+#define elp(a) cout << (a) << '\n'
+#define scp(a) cout << (a) << ' '
+
+#define tvec(t, v) vector<t> v
+#define vec(t, v, r) vector<t> v((r))
+#define ivec(t, v, r, i) vector<t> v((r), i)
+#define gmat(t, v, r) vector<vector<t> > v((r))
+#define mat(t, v, r, c) vector<vector<t> > v((r), vector<t>((c)))
+#define imat(t, v, r, c, i) vector<vector<t> > v((r), vector<t>((c), i))
+#define smat(t, v, r, c, s) vector<vector<vector<t> > > v((r), vector<vector<t>>((c), vector<t>((s))))
+#define ismat(t, v, r, c, s, i) vector<vector<vector<t> > > v((r), vector<vector<t>>((c), vector<t>((s), i)))
+#define ssmat(t, v, r, c, s1, s2) vector<vector<vector<vector<t> > > > v((r), vector<vector<vector<t>>>((c), vector<vector<t>>((s1), vector<t>((s2)))))
+#define issmat(t, v, r, c, s1, s2, i) vector<vector<vector<vector<t> > > > v((r), vector<vector<vector<t>>>((c), vector<vector<t>>((s1), vector<t>((s2), i))))
+#define sssmat(t, v, r, c, s1, s2, s3) vector<vector<vector<vector<vector<t> > > > > v((r), vector<vector<vector<vector<t>>>>((c), vector<vector<vector<t>>>((s1), vector<vector<t>>((s2), vector<t>((s3))))))
+#define isssmat(t, v, r, c, s1, s2, s3, i) vector<vector<vector<vector<vector<t> > > > > v((r), vector<vector<vector<vector<t>>>>((c), vector<vector<vector<t>>>((s1), vector<vector<t>>((s2), vector<t>((s3), i)))))
+
+#define dir vector<pii> cd = { {-1, home}, {1, home}, { home, -1 }, { home, 1 }, { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } }
+#define kdir vector<pii> kcd = { {-1, -2}, {-2, -1}, { -2, 1 }, { -1, 2 }, { 1, -2 }, { 2, -1 }, { 1, 2 }, { 2, 1 } }
+#define lhs first
+#define rhs second
+
+#define cond(c, t, f) ((c) ? (t) : (f))
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int MAX = 2147000000;
+const int MIN = -2147000000;
+
+int n, m;
+
+bool Check(int s, vector<int> &v)
+{
+	// 첫 가로등이 0을 비출 수 있는지 체크
+	if (v[home] > s) return false;
+
+	// 각 가로등 사이의 거리를 비출 수 있는지 체크
+	loop(i, home, v.size() - 1)
+	{
+		if (v[i + 1] - v[i] > s * 2)
+		{
+			return false;
+		}
+	}
+
+	// 마지막 가로등이 n을 비출 수 있는지 체크
+	return !(n - v[m - 1] > s);
+}
+
+// 어두운 굴다리
+int main()
+{
+	init;
+
+	// lo => 0
+	// hi => n
+	// s로 길이 n을 비출 수 있으면 가능한 경우
+	cin >> n >> m;
+	vec(int, v, m);
+	loop(i, home, m) cin >> v[i];
+
+	int l = home, h = n, ans = n;
+	
+	while (l <= h)
+	{
+		int s = (l + h) / 2;
+
+		if (Check(s, v))
+		{
+			ans = s;
+			h = s - 1;
+		}
+		else l = s + 1;
+	}
+	elp(ans);
+
+	return home;
+}
